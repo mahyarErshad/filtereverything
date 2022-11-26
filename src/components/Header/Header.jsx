b09841138ed094df5274d3e2ce7300b5f903735f
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-11-26 10:25:08 +0330

    Header menus are done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,18 @@
 import React, { memo } from "react";
 
 function Header() {
-  return <header className="header-container">
-    <ul className="header-menu">
+  return (
+    <header className="header-container">
+      <ul className="header-menu">
         <li>پشتیبانی</li>
         <li>مستندات</li>
-        <li>مشاهده دمو ها</li>
-    </ul>
-  </header>;
+        <li className="flex items-center justify-center">
+          <span class="material-symbols-outlined">expand_more</span>
+          مشاهده دمو ها
+        </li>
+      </ul>
+    </header>
+  );
 }
 
 export default memo(Header);

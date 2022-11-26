21cde7f586da78f04e39f2deb85d29295ecf9666
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-11-26 10:10:29 +0330

    Changed height from % to vh

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,9 @@ import React, { memo } from "react";
 function Header() {
   return <header className="header-container">
     <ul className="header-menu">
-        <li>Hi</li>
+        <li>پشتیبانی</li>
+        <li>مستندات</li>
+        <li>مشاهده دمو ها</li>
     </ul>
   </header>;
 }

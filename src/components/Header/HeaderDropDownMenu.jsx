9fdbb7c281f521aada3aec6159d47cbccf7d64b8
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-11-27 10:26:32 +0330

    Header's UI has been changed

--- src/components/Header/HeaderDropDownMenu.jsx
+++ src/components/Header/HeaderDropDownMenu.jsx
@@ -3,12 +3,12 @@ export default function HeaderDropDownMenu() {
     <>
       <li className="relative z-10 text-base overflow-hidden hover:overflow-visible cursor-pointer ease-linear duration-[200ms] text-center h-full text-white flex items-start px-[0.5rem] hover:text-primary]">
         <div className="flex justify-center items-center">
-          <span className="material-symbols-outlined">expand_more</span>
           <span>مشاهده دمو ها</span>
+          <span className="material-symbols-outlined">expand_more</span>
         </div>
         <ul className="absolute right-[10%] top-[100%] drop-down-menu flex justify-start items-start flex-col">
-          <div className="flex justify-start items-start flex-col px-4 py-6">
-            <span className="drop-down-menu__item">فیلتر کردن پست ها</span>
+          <div className="flex justify-start items-start flex-col px-4 py-6 w-max">
+            <span className="drop-down-menu__item mb-2">فیلتر کردن پست ها</span>
             <li className="header-submenu">پوشاک زنانه</li>
             <li className="header-submenu">مثال های سئو</li>
             <li className="header-submenu">لپتاپ</li>

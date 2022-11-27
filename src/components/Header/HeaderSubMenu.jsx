f76a46d70a65576e9f5d649c5251a42489ffff76
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-11-27 11:43:52 +0330

    Component has initialized

--- src/components/Header/HeaderSubMenu.jsx
+++ src/components/Header/HeaderSubMenu.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
-function HeaderSubMenu() {
+function HeaderSubMenu({title , data=[]}) {
   return (
     <div className="flex justify-start items-start flex-col p-4 w-max">
-    <span className="drop-down-menu__item mb-2">فیلتر کردن پست ها</span>
-    <li className="header-submenu">پوشاک زنانه</li>
+    <span className="drop-down-menu__item mb-2">{title}</span>
+    {
+        data.map((item)=>{
+            return     <li key={data.text} className="header-submenu">{data.text}</li>
+        })
+    }
+
     <li className="header-submenu">مثال های سئو</li>
     <li className="header-submenu">لپتاپ</li>
     <li className="header-submenu">تلویزیون</li>

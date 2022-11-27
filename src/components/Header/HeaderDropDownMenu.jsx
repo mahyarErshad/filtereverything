05b204fcd07134462ab484f5c2a30eeedabd25be
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-11-27 09:11:07 +0330

    Dropdown menu is working without packages

--- src/components/Header/HeaderDropDownMenu.jsx
+++ src/components/Header/HeaderDropDownMenu.jsx
@@ -1,23 +1,24 @@
 export default function HeaderDropDownMenu() {
   return (
     <>
-      <li className="flex items-center justify-center">
-        <span className="material-symbols-outlined">expand_more</span>
-        مشاهده دمو ها
-      </li>
-
-      <div className="flex justify-between items-center flex-col">
-        <ul>
-          <li className="header-submenu">پوشاک زنانه</li>
-          <li className="header-submenu">مثال های سئو</li>
-          <li className="header-submenu">لپتاپ</li>
-          <li className="header-submenu">تلویزیون</li>
-          <li className="header-submenu">مثال های جستجوی محصول</li>
+      <li className="relative z-10 text-base overflow-hidden hover:overflow-visible cursor-pointer ease-linear duration-[200ms] text-center h-full text-white flex items-start px-[0.5rem] hover:text-primary]">
+        <div className="flex justify-center items-center">
+          <span className="material-symbols-outlined">expand_more</span>
+          <span>مشاهده دمو ها</span>
+        </div>
+        <ul className="absolute right-[0] top-[100%] drop-down-menu flex justify-start items-start flex-col">
+          <div className="drop-down-menu__item">
+            فیلتر کردن پست ها
+            <li className="header-submenu">پوشاک زنانه</li>
+            <li className="header-submenu">مثال های سئو</li>
+            <li className="header-submenu">لپتاپ</li>
+            <li className="header-submenu">تلویزیون</li>
+            <li className="header-submenu">مثال های جستجوی محصول</li>
+          </div>
+          <div className="drop-down-menu__item">مثال فیلترها</div>
+          <div className="drop-down-menu__item">شخصی سازی</div>
         </ul>
-        <div>فیلتر کردن پست ها</div>
-        <div>مثال فیلترها</div>
-        <div>شخصی سازی</div>
-      </div>
+      </li>
     </>
   );
 }

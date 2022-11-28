e17cbc27d0144d251e22719c44d14232b0e060ac
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-11-28 12:37:02 +0330

    Hamburger button is alive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,20 @@
-import React, { memo } from "react";
+import React, { memo, useState } from "react";
 import HeaderDropDownMenu from "./HeaderDropDownMenu";
 
 function Header() {
+  const [openMenu, setOpenMenu] = useState(false);
   return (
     <header className="header-container">
       <ul className="header-menu flex justify-between max-sm:px-4 sm:px-[10%] md:px-[20%] text-prima">
-        <div className="flex justify-center gap-6 h-full items-center max-sm:hidden sm:hidden md:flex">
+        <div className={`flex justify-center gap-6 h-full items-center max-sm:${openMenu ? "flex" : "hidden"} sm:${openMenu ? "flex" : "hidden"} md:flex`}>
           <li className="h-full flex items-center">پشتیبانی</li>
           <li className="h-full flex items-center">مستندات</li>
           <HeaderDropDownMenu />
         </div>
         <div className="sm:visible md:hidden flex items-center justify-center cursor-pointer">
-          <button class="material-symbols-outlined text-white hover:text-slate-300">menu</button>
+          <button onClick={() => setOpenMenu((prev) => !prev)} class="material-symbols-outlined text-white hover:text-slate-300">
+            menu
+          </button>
         </div>
       </ul>
     </header>

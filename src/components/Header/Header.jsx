4f6db6228d60a5a57b2ff33cd1e51ea9c3242270
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-11-28 13:17:17 +0330

    Button icon is conditional

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,10 +8,10 @@ function Header() {
       <ul className="header-menu flex justify-between max-sm:px-4 sm:px-[10%] md:px-[20%] text-prima relative">
         <div className="sm:visible md:hidden flex items-center justify-center cursor-pointer">
           <button onClick={() => setOpenMenu((prev) => !prev)} class="material-symbols-outlined text-white hover:text-[#6efafb]">
-            menu
+            {openMenu ? "close" : "menu" }
           </button>
         </div>
-        <div className={`flex justify-center gap-6 h-full items-center max-sm:${openMenu ? "flex" : "hidden"} sm:${openMenu ? "flex" : "hidden"} md:flex max-sm:flex-col sm:flex-col md:flex-row sm:absolute max-sm:absolute max-sm:top-[125%] max-sm:right-[0%] sm:top-[125%] sm:right-[8%] md:top-[0%] `}>
+        <div className={`flex justify-center gap-6 h-full items-center max-sm:${openMenu ? "flex" : "hidden"} sm:${openMenu ? "flex" : "hidden"} md:flex max-sm:flex-col sm:flex-col md:flex-row sm:absolute max-sm:absolute max-sm:top-[140%] max-sm:right-[0%] sm:top-[140%] sm:right-[8%] md:top-[0%] `}>
           <li className="h-full flex items-center">پشتیبانی</li>
           <li className="h-full flex items-center">مستندات</li>
           <HeaderDropDownMenu />

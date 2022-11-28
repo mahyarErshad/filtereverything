ca36d416924281d641ed59784325d159a0d48784
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-11-28 13:52:43 +0330

    Fixed a UI bug

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,13 +5,13 @@ function Header() {
   const [openMenu, setOpenMenu] = useState(false);
   return (
     <header className="header-container">
-      <ul className="header-menu flex justify-between max-sm:px-4 sm:px-[10%] md:px-[20%] text-prima relative ">
+      <ul className="header-menu flex justify-between max-sm:px-4 sm:px-[10%] md:px-[20%] text-prima relative h-16 ">
         <div className="sm:visible md:hidden flex items-center justify-center cursor-pointer">
-          <button onClick={() => setOpenMenu((prev) => !prev)} class="material-symbols-outlined text-white hover:text-[#6efafb]">
-            {openMenu ? "close" : "menu" }
+          <button onClick={() => setOpenMenu((prev) => !prev)} className="material-symbols-outlined text-white hover:text-[#6efafb]">
+            {openMenu ? "close" : "menu"}
           </button>
         </div>
-        <div className={`flex justify-center gap-6 h-full items-center max-sm:${openMenu ? "flex" : "hidden"} sm:${openMenu ? "flex" : "hidden"} md:flex max-sm:flex-col sm:flex-col md:flex-row sm:absolute max-sm:absolute max-sm:top-[140%] max-sm:right-[0%] sm:top-[140%] sm:right-[8%] md:top-[0%] `}>
+        <div className={`flex justify-center gap-6 md:h-full sm:h-auto max-sm:h-auto items-center max-sm:${openMenu ? "flex" : "hidden"} sm:${openMenu ? "flex" : "hidden"} md:flex max-sm:flex-col sm:flex-col md:flex-row sm:absolute max-sm:absolute max-sm:top-[140%] max-sm:right-[0%] sm:top-[140%] sm:right-[8%] md:top-[0%] `}>
           <li className="h-full flex items-center">پشتیبانی</li>
           <li className="h-full flex items-center">مستندات</li>
           <HeaderDropDownMenu />

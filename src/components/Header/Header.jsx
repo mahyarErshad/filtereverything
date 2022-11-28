d85995355ef6bbded4c5c368c718b008394d8281
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-11-28 10:52:24 +0330

    Working on px of menus

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,14 +4,14 @@ import HeaderDropDownMenu from "./HeaderDropDownMenu";
 function Header() {
   return (
     <header className="header-container">
-      <ul className="header-menu flex justify-between max-sm:px-4 md:px-[20%]">
+      <ul className="header-menu flex justify-between max-sm:px-4 sm:px-[10%] md:px-[20%]">
         <div className="flex justify-center gap-6 h-full items-center max-sm:hidden">
           <li className="h-full flex items-center">پشتیبانی</li>
           <li className="h-full flex items-center">مستندات</li>
           <HeaderDropDownMenu />
         </div>
         <div className="sm:visible md:hidden">
-          <button>Hi</button>
+          <button className="text-white">Hi</button>
         </div>
       </ul>
     </header>

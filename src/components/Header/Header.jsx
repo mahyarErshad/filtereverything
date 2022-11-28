95f32b6d73df1678074e57cf9b891ad91c34467d
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-11-28 13:09:16 +0330

    Menu is responsive on every size

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,16 +5,16 @@ function Header() {
   const [openMenu, setOpenMenu] = useState(false);
   return (
     <header className="header-container">
-      <ul className="header-menu flex justify-between max-sm:px-4 sm:px-[10%] md:px-[20%] text-prima">
-        <div className={`flex justify-center gap-6 h-full items-center max-sm:${openMenu ? "flex" : "hidden"} sm:${openMenu ? "flex" : "hidden"} md:flex max-sm:flex-col sm:flex-col md:flex-row`}>
-          <li className="h-full flex items-center">پشتیبانی</li>
-          <li className="h-full flex items-center">مستندات</li>
-          <HeaderDropDownMenu />
-        </div>
+      <ul className="header-menu flex justify-between max-sm:px-4 sm:px-[10%] md:px-[20%] text-prima relative">
         <div className="sm:visible md:hidden flex items-center justify-center cursor-pointer">
           <button onClick={() => setOpenMenu((prev) => !prev)} class="material-symbols-outlined text-white hover:text-[#6efafb]">
             menu
           </button>
+        </div>
+        <div className={`flex justify-center gap-6 h-full items-center max-sm:${openMenu ? "flex" : "hidden"} sm:${openMenu ? "flex" : "hidden"} md:flex max-sm:flex-col sm:flex-col md:flex-row sm:absolute max-sm:absolute max-sm:top-[125%] max-sm:right-[0%] sm:top-[125%] sm:right-[8%] md:top-[0%] `}>
+          <li className="h-full flex items-center">پشتیبانی</li>
+          <li className="h-full flex items-center">مستندات</li>
+          <HeaderDropDownMenu />
         </div>
       </ul>
     </header>

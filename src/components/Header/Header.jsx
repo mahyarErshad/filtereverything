d66384eae73737a043542826c5681610ec4f5347
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-11-28 12:22:52 +0330

    Added hamburger menu icon

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,14 +4,14 @@ import HeaderDropDownMenu from "./HeaderDropDownMenu";
 function Header() {
   return (
     <header className="header-container">
-      <ul className="header-menu flex justify-between max-sm:px-4 sm:px-[10%] md:px-[20%]">
-        <div className="flex justify-center gap-6 h-full items-center max-sm:hidden">
+      <ul className="header-menu flex justify-between max-sm:px-4 sm:px-[10%] md:px-[20%] text-prima">
+        <div className="flex justify-center gap-6 h-full items-center max-sm:hidden sm:hidden md:flex">
           <li className="h-full flex items-center">پشتیبانی</li>
           <li className="h-full flex items-center">مستندات</li>
           <HeaderDropDownMenu />
         </div>
-        <div className="sm:visible md:hidden">
-          <button className="text-white">Hi</button>
+        <div className="sm:visible md:hidden flex items-center justify-center cursor-pointer">
+          <span class="material-symbols-outlined text-white hover:text-slate-300">menu</span>
         </div>
       </ul>
     </header>

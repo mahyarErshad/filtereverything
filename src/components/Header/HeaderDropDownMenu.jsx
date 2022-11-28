266ecf7790b6ddc4d462497ded46ff4a6dc24b51
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-11-28 16:02:26 +0330

    Header has some bugs to be fixed

--- src/components/Header/HeaderDropDownMenu.jsx
+++ src/components/Header/HeaderDropDownMenu.jsx
@@ -2,6 +2,7 @@ import React, { memo } from "react";
 import HeaderSubMenu from "./HeaderSubMenu";
 
 function HeaderDropDownMenu() {
+
   return (
     <>
       <li className="relative z-10 text-base overflow-hidden hover:overflow-visible cursor-pointer ease-linear duration-[200ms] text-center h-full text-white flex items-center px-[0.5rem] hover:text-primary]">
@@ -9,7 +10,7 @@ function HeaderDropDownMenu() {
           <span>مشاهده دمو ها</span>
           <span className="material-symbols-outlined">expand_more</span>
         </div>
-        <ul className="absolute right-[5%] top-[100%] drop-down-menu flex justify-start items-start flex-row">
+        <ul className="absolute right-[5%] top-[100%] drop-down-menu flex justify-start items-start flex-row max-sm:flex-col max-sm:overflow-auto max-sm:h-[50vh]">
           <div>
             <HeaderSubMenu title="فیلتر کردن پستها" data={["پوشاک زنانه", "مثال های سئو", "کامپیوتر", "لپتاپ", "مثال های جستجوی محصول"]} />
             <HeaderSubMenu title="مثال فیلترها" data={["قیمت", "حراج", "وزن", "لپتاپ", "قابل دانلود", "امتیاز محصول", "تعداد فروش", "نویسنده"]} />

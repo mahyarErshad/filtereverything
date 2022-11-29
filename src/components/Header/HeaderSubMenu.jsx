48a16ff023e9c477b1d1b4931888f970741622e9
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-11-29 10:27:36 +0330

    Defined a showMenu state

--- src/components/Header/HeaderSubMenu.jsx
+++ src/components/Header/HeaderSubMenu.jsx
@@ -1,6 +1,7 @@
-import React, { memo } from "react";
+import React, { memo, useState } from "react";
 
 function HeaderSubMenu({ title, data = [] }) {
+  const [showMenu, setShowMenu] = useState(false)
   return (
     <div className="flex justify-start items-start flex-col p-4 w-max">
       <span className="drop-down-menu__item mb-2">{title}</span>

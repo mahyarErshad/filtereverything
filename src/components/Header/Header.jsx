e54bb0a155b2e19971e4fb3665bc89640d7851a9
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-11-29 09:35:33 +0330

    Top position has been corrected

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,7 +13,7 @@ function Header() {
             {openMenu ? "close" : "menu"}
           </button>
         </div>
-        <div className={`flex justify-center max-sm:items-start max-md:w-full max-md:bg-red-500 sm:pr-[6.5rem] max-sm:pr-12 sm:items-start gap-6 md:h-full sm:h-auto max-sm:h-auto items-center max-sm:${openMenuClass} sm:${openMenuClass} md:flex max-sm:flex-col sm:flex-col md:flex-row sm:absolute max-sm:absolute max-sm:top-[140%] max-sm:right-[0%] sm:top-[140%] sm:right-[0] md:top-[0%] `}>
+        <div className={`flex justify-center max-sm:items-start max-md:w-full max-md:bg-red-500 sm:pr-[6.5rem] max-sm:pr-12 sm:items-start gap-6 md:h-full sm:h-auto max-sm:h-auto items-center max-sm:${openMenuClass} sm:${openMenuClass} md:flex max-sm:flex-col sm:flex-col md:flex-row sm:absolute max-sm:absolute max-sm:top-[100%] max-sm:right-[0%] sm:top-[100%] sm:right-[0] md:top-[0%] `}>
           <li className="h-full flex items-center">پشتیبانی</li>
           <li className="h-full flex items-center">مستندات</li>
           <HeaderDropDownMenu />

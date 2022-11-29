0cc196d6425c56fffdaf4c5ba270dab99120f675
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-11-29 10:49:01 +0330

    Head lines catching color on click

--- src/components/Header/HeaderSubMenu.jsx
+++ src/components/Header/HeaderSubMenu.jsx
@@ -3,9 +3,10 @@ import React, { memo, useState } from "react";
 function HeaderSubMenu({ title, data = [] }) {
   const [showMenu, setShowMenu] = useState(false)
   const showMenuClass = showMenu ? 'visible' : 'hidden';
+  const textColor = showMenu ? 'text-[#6efafb]' : 'text-white';
   return (
     <div className="flex justify-start items-start flex-col p-4 w-max">
-      <span onClick={()=> setShowMenu((prev)=> !prev)} className={`drop-down-menu__item mb-2 flex items-center ${showMenu ? "text-[#6efafb]" : "text-white"}`}>{title}
+      <span onClick={()=> setShowMenu((prev)=> !prev)} className={`drop-down-menu__item mb-2 flex items-center ${textColor}`}>{title}
       <span className="material-symbols-outlined max-md:visible md:hidden">expand_more</span>
       </span>
       {data.map((item) => {

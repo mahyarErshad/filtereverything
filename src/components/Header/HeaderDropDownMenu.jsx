d45530b98e1f11978a9fc615a69a8e6f8fc958c8
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-11-29 10:14:59 +0330

    HeaderDropDownMenu is more responsive

--- src/components/Header/HeaderDropDownMenu.jsx
+++ src/components/Header/HeaderDropDownMenu.jsx
@@ -5,12 +5,12 @@ function HeaderDropDownMenu() {
 
   return (
     <>
-      <li className="relative z-10 text-base overflow-hidden hover:overflow-visible cursor-pointer ease-linear duration-[200ms] text-center h-full text-white flex items-center px-[0.5rem] hover:text-primary]">
+      <li className="relative z-10 text-base overflow-hidden hover:overflow-visible max-md:w-full cursor-pointer ease-linear duration-[200ms] text-center h-full text-white flex items-center px-[0.5rem] hover:text-primary]">
         <div className="flex justify-center items-center">
           <span>مشاهده دمو ها</span>
           <span className="material-symbols-outlined">expand_more</span>
         </div>
-        <ul className="absolute right-[5%] top-[100%] drop-down-menu flex justify-start items-start flex-row max-sm:flex-col max-sm:overflow-auto max-sm:h-[50vh]">
+        <ul className="absolute md:right-[5%] max-md:right-0 top-[100%] max-md:w-full max-md:pr-0 drop-down-menu flex justify-start items-start flex-row max-sm:flex-col max-sm:overflow-auto max-sm:h-[50vh]">
           <div>
             <HeaderSubMenu title="فیلتر کردن پستها" data={["پوشاک زنانه", "مثال های سئو", "کامپیوتر", "لپتاپ", "مثال های جستجوی محصول"]} />
             <HeaderSubMenu title="مثال فیلترها" data={["قیمت", "حراج", "وزن", "لپتاپ", "قابل دانلود", "امتیاز محصول", "تعداد فروش", "نویسنده"]} />

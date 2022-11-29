a4f115e7f64fafa12d58c8947118e9fe85a698d3
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-11-29 10:30:51 +0330

    Defined a function for show and hide the menu

--- src/components/Header/HeaderSubMenu.jsx
+++ src/components/Header/HeaderSubMenu.jsx
@@ -5,7 +5,7 @@ function HeaderSubMenu({ title, data = [] }) {
   const showMenuClass = showMenu ? 'visible' : 'hidden';
   return (
     <div className="flex justify-start items-start flex-col p-4 w-max">
-      <span className="drop-down-menu__item mb-2">{title}</span>
+      <span onClick={()=> setShowMenu((prev)=> !prev)} className="drop-down-menu__item mb-2">{title}</span>
       {data.map((item) => {
         return (
           <li key={item} className={`header-submenu max-md:${showMenuClass}`}>

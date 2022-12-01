5fd355ba38eaa49b0aa7a8f33716aefe5c23ca86
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-01 09:47:29 +0330

    Giving default text color

--- src/components/Utils/Button/Button.jsx
+++ src/components/Utils/Button/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Button(props) {
   const { title } = props;
   return (
-    <button className={`bg-primary py-2 px-4 rounded-md ${props.className}`} onClick={props.onClick}>
+    <button className={`bg-primary text-white py-2 px-4 rounded-md ${props.className}`} onClick={props.onClick}>
       {title}
     </button>
   );

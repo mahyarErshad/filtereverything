5d60b9cbe3fe1974ebc163a31e101b26f2e5f650
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-01 09:57:43 +0330

    Worked on button on hover

--- src/components/Utils/Button/Button.jsx
+++ src/components/Utils/Button/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Button(props) {
   const { title } = props;
   return (
-    <button className={`bg-primary text-black py-2 px-4 rounded-md text-ellipsis whitespace-nowrap ${props.className}`} onClick={props.onClick}>
+    <button className={`bg-primary text-black py-2 px-4 rounded-md text-ellipsis whitespace-nowrap ${props.className} hover:bg-[#1ecece]`} onClick={props.onClick}>
       {title}
     </button>
   );

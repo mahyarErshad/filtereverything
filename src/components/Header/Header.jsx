64a9baab94157a0a3c227877e56a6912b887975f
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-01 09:53:31 +0330

    Button has been added and header is responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,19 +7,21 @@ function Header() {
   const openMenuClass = openMenu ? "visible" : "hidden";
   return (
     <header className="header-container">
-      <ul className="header-menu flex justify-between max-sm:px-4 sm:px-[10%] md:px-[20%] text-prima relative h-16 ">
-        <div className="sm:visible md:hidden flex items-center justify-center cursor-pointer">
-          <button onClick={() => setOpenMenu((prev) => !prev)} className="material-symbols-outlined text-white hover:text-[#6efafb]">
-            {openMenu ? "close" : "menu"}
-          </button>
+      <div className="header-menu flex justify-between max-sm:px-4 sm:px-[10%] md:px-[20%] text-prima relative h-16 ">
+        <div className="flex justify-between w-full">
+          <div className="sm:visible md:hidden flex items-center justify-center cursor-pointer">
+            <button onClick={() => setOpenMenu((prev) => !prev)} className="material-symbols-outlined text-white hover:text-[#6efafb]">
+              {openMenu ? "close" : "menu"}
+            </button>
+          </div>
+          <div className={`flex justify-center max-sm:items-start max-md:pb-2 max-md:w-full max-md:bg-[#333333] max-md:opacity-[0.8] sm:pr-[6.5rem] max-sm:pr-12 sm:items-start gap-6 md:h-full sm:h-auto max-sm:h-auto items-center max-md:${openMenuClass} md:flex max-sm:flex-col sm:flex-col md:flex-row sm:absolute max-sm:absolute max-sm:top-[100%] max-sm:right-[0%] sm:top-[100%] sm:right-[0] md:top-[0%] `}>
+            <li className="h-full text-[1.125rem] animation-fade font-bold flex items-center">پشتیبانی</li>
+            <li className="h-full text-[1.125rem] animation-fade font-bold flex items-center">مستندات</li>
+            <HeaderDropDownMenu />
+          </div>
         </div>
-        <div className={`flex justify-center max-sm:items-start max-md:pb-2 max-md:w-full max-md:bg-[#333333] max-md:opacity-[0.8] sm:pr-[6.5rem] max-sm:pr-12 sm:items-start gap-6 md:h-full sm:h-auto max-sm:h-auto items-center max-md:${openMenuClass} md:flex max-sm:flex-col sm:flex-col md:flex-row sm:absolute max-sm:absolute max-sm:top-[100%] max-sm:right-[0%] sm:top-[100%] sm:right-[0] md:top-[0%] `}>
-          <li className="h-full text-[1.125rem] animation-fade font-bold flex items-center">پشتیبانی</li>
-          <li className="h-full text-[1.125rem] animation-fade font-bold flex items-center">مستندات</li>
-          <HeaderDropDownMenu />
-        </div>
-      </ul>
-      <Button title="خرید نسخه pro" />
+        <Button title="خرید نسخه pro" />
+      </div>
     </header>
   );
 }
--- src/components/Utils/Button/Button.jsx
+++ src/components/Utils/Button/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Button(props) {
   const { title } = props;
   return (
-    <button className={`bg-primary text-white py-2 px-4 rounded-md ${props.className}`} onClick={props.onClick}>
+    <button className={`bg-primary text-black py-2 px-4 rounded-md text-ellipsis whitespace-nowrap ${props.className}`} onClick={props.onClick}>
       {title}
     </button>
   );

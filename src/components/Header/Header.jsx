3ef55262282f34a2628562d364e7bb12a4290158
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-03 12:27:58 +0330

    Translated the pro word

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -21,7 +21,7 @@ function Header() {
             <HeaderDropDownMenu />
           </div>
         </div>
-        <Button className="font-bold hover:bg-[#18caca]" title="خرید نسخه pro" />
+        <Button className="font-bold hover:bg-[#18caca]" title="خرید نسخه حرفه ای" />
       </div>
       <HeaderTextSection />
     </header>

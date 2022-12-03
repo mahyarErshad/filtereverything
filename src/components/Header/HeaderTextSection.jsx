dc7c99d5e0ab2f2bb7c6aad4994e6a0c887379df
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-03 11:17:02 +0330

    Top text is written and styled

--- src/components/Header/HeaderTextSection.jsx
+++ src/components/Header/HeaderTextSection.jsx
@@ -3,7 +3,8 @@ import React, { memo } from "react";
 function HeaderTextSection() {
   return (
     <div className="md:pr-[20%] sm:pr-10 max-sm:pr-4 pt-16">
-      <h1 className="md:text-[4.5rem] sm:text-[3.5rem] max-sm:text-[3rem] text-white font-bold">غول</h1>
+      <h1 className="md:text-[4.5rem] sm:text-[3.5rem] max-sm:text-[3rem] text-white font-bold">غول
+      <sup className="text-center text-black py-0 px-4 rounded-[35px] bg-[#ffd000] text-4xl mr-4">+سئو</sup></h1>
       <h2 className="md:text-[4.5rem] sm:text-[3.5rem] max-sm:text-[3rem] text-white font-bold mr-3">فیلتر</h2>
     </div>
   );

46a2b2854ab44e25e2ed644b2f5d851143799a14
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-03 10:18:47 +0330

    Font size is responsive

--- src/components/Header/HeaderTextSection.jsx
+++ src/components/Header/HeaderTextSection.jsx
@@ -1,11 +1,12 @@
-import React, { memo } from 'react'
+import React, { memo } from "react";
 
 function HeaderTextSection() {
   return (
-    <div>
-
+    <div className="">
+      <h1 className="md:text-[4.5rem] sm:text-[3.5rem] max-sm:text-[3rem] text-white font-bold">غول</h1>
+      <h2 className="md:text-[4.5rem] sm:text-[3.5rem] max-sm:text-[3rem] text-white font-bold mr-3">فیلتر</h2>
     </div>
-  )
+  );
 }
 
-export default memo(HeaderTextSection)
+export default memo(HeaderTextSection);

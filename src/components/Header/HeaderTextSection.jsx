161bf683d3dae54c3beed50e08a879abbbca3884
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-03 10:20:30 +0330

    padding right is responsive

--- src/components/Header/HeaderTextSection.jsx
+++ src/components/Header/HeaderTextSection.jsx
@@ -2,7 +2,7 @@ import React, { memo } from "react";
 
 function HeaderTextSection() {
   return (
-    <div className="">
+    <div className="md:pr-[20%] sm:pr-10 max-sm:pr-4">
       <h1 className="md:text-[4.5rem] sm:text-[3.5rem] max-sm:text-[3rem] text-white font-bold">غول</h1>
       <h2 className="md:text-[4.5rem] sm:text-[3.5rem] max-sm:text-[3rem] text-white font-bold mr-3">فیلتر</h2>
     </div>

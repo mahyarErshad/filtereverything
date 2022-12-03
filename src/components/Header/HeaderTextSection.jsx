b9dc4c07fe0346a327cc003144c70a32af0f5a2f
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-03 12:20:52 +0330

    SEO is smaller

--- src/components/Header/HeaderTextSection.jsx
+++ src/components/Header/HeaderTextSection.jsx
@@ -1,5 +1,5 @@
 import React, { memo } from "react";
-import {ReactComponent as woocommerceLogo} from "../../assets/images/woocommerce_logo.svg";
+import {ReactComponent as WoocommerceLogo} from "../../assets/images/woocommerce_logo.svg";
 
 function HeaderTextSection() {
   return (
@@ -7,13 +7,13 @@ function HeaderTextSection() {
       <div className="md:pr-[20%] sm:pr-10 max-sm:pr-4 pt-16">
         <h1 className="md:text-[4.5rem] sm:text-[3.5rem] max-sm:text-[3rem] text-white font-bold">
           غول
-          <sup className="text-center text-black py-0 px-4 rounded-[35px] bg-[#ffd000] text-4xl mr-4">+سئو</sup>
+          <sup className="text-center text-black py-0 px-4 rounded-[35px] bg-[#ffd000] text-3xl mr-4">+سئو</sup>
         </h1>
         <h2 className="md:text-[4.5rem] sm:text-[3.5rem] max-sm:text-[3rem] text-white font-bold mr-3">فیلتر</h2>
       </div>
       <div>
-        <p>برای WordPress</p>
-        <p>و WordPress</p>
+        <p className="max-md:text-3xl md:text-4xl font-bold text-white">برای WordPress</p>
+        <div className="max-md:text-3xl md:text-4xl font-bold text-white flex items-center"><span>و</span> <WoocommerceLogo className="max-md:w-7 md:w-9" /></div>
       </div>
     </>
   );

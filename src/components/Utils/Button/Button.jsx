c30e089c7c5b040dfe1ec31c3153a77918e702a4
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-03 09:01:19 +0330

    backgroundColor = props.backgroundColor || "bg-primary"

--- src/components/Utils/Button/Button.jsx
+++ src/components/Utils/Button/Button.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 function Button(props) {
   const { title } = props;
+  const backgroundColor = props.backgroundColor || "bg-primary"
   return (
     <button className={`bg-primary text-black py-2 px-4 rounded-md text-ellipsis whitespace-nowrap ${props.className}`} onClick={props.onClick}>
       {title}

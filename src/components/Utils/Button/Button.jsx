a4eeb15059181cd2856f6de183cdc924ac654188
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-03 09:09:25 +0330

    textColor = props.textColor || "text-black"

--- src/components/Utils/Button/Button.jsx
+++ src/components/Utils/Button/Button.jsx
@@ -3,8 +3,9 @@ import React from "react";
 function Button(props) {
   const { title } = props;
   const backgroundColor = props.backgroundColor || "bg-primary"
+  const textColor = props.textColor || "text-black"
   return (
-    <button className={`${backgroundColor} text-black py-2 px-4 rounded-md text-ellipsis whitespace-nowrap ${props.className}`} onClick={props.onClick}>
+    <button className={`${backgroundColor} ${textColor} py-2 px-4 rounded-md text-ellipsis whitespace-nowrap ${props.className}`} onClick={props.onClick}>
       {title}
     </button>
   );

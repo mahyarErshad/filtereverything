cd2c20904cfbe99627073617bba983a2d326dceb
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-03 12:05:07 +0330

    Added woocommerce_logo as svg

--- src/components/Header/HeaderTextSection.jsx
+++ src/components/Header/HeaderTextSection.jsx
@@ -1,12 +1,21 @@
 import React, { memo } from "react";
+import woocommerceLogo from "../../assets/images/woocommerce_logo.svg";
 
 function HeaderTextSection() {
   return (
-    <div className="md:pr-[20%] sm:pr-10 max-sm:pr-4 pt-16">
-      <h1 className="md:text-[4.5rem] sm:text-[3.5rem] max-sm:text-[3rem] text-white font-bold">غول
-      <sup className="text-center text-black py-0 px-4 rounded-[35px] bg-[#ffd000] text-4xl mr-4">+سئو</sup></h1>
-      <h2 className="md:text-[4.5rem] sm:text-[3.5rem] max-sm:text-[3rem] text-white font-bold mr-3">فیلتر</h2>
-    </div>
+    <>
+      <div className="md:pr-[20%] sm:pr-10 max-sm:pr-4 pt-16">
+        <h1 className="md:text-[4.5rem] sm:text-[3.5rem] max-sm:text-[3rem] text-white font-bold">
+          غول
+          <sup className="text-center text-black py-0 px-4 rounded-[35px] bg-[#ffd000] text-4xl mr-4">+سئو</sup>
+        </h1>
+        <h2 className="md:text-[4.5rem] sm:text-[3.5rem] max-sm:text-[3rem] text-white font-bold mr-3">فیلتر</h2>
+      </div>
+      <div>
+        <p>برای WordPress</p>
+        <p>و WordPress</p>
+      </div>
+    </>
   );
 }
 

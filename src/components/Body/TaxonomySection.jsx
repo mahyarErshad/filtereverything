c97523b72ef0067debc35b5cb61af62ab265b188
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-05 12:37:12 +0330

    Texts are responsive

--- src/components/Body/TaxonomySection.jsx
+++ src/components/Body/TaxonomySection.jsx
@@ -4,10 +4,10 @@ import taxonomyImage from "../../assets/images/taxonomy.jpg";
 function TaxonomySection() {
   return (
     <section className="flex justify-between flex-nowrap gap-4 w-full px-4 max-md:flex-col">
-      <div className="md:w-2/4 max-md:w-full flex max-md:justify-center bg-yellow-300 md:pt-14">
-        <div className="flex flex-col items-end w-full pl-6">
-        <p className="font-bold text-3xl">فیلترها بر اساس طبقه بندی</p>
-        <p className="font-bold text-3xl">و فیلدهای سفارشی</p>
+      <div className="md:w-2/4 max-md:w-full flex max-md:justify-center bg-yellow-300 md:pt-20">
+        <div className="flex flex-col w-full gap-4 max-lg:px-4 lg:px-[10%]">
+        <p className="font-bold max-lg:text-xl lg:text-3xl self-center break-keep">فیلترها بر اساس طبقه بندی</p>
+        <p className="font-bold max-lg:text-xl lg:text-3xl max-lg:self-center lg:self-end lg:ml-[10%]">و فیلدهای سفارشی</p>
         </div>
       </div>
       <div className="md:w-2/4 max-md:w-full flex max-md:justify-center md:justify-start">

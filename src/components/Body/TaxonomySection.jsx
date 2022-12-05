f93640673553cfb0580e93dfa1badb07eb68bc7a
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-05 10:18:08 +0330

    Changed the text and image order

--- src/components/Body/TaxonomySection.jsx
+++ src/components/Body/TaxonomySection.jsx
@@ -3,12 +3,13 @@ import taxonomyImage from "../../assets/images/taxonomy.jpg";
 
 function TaxonomySection() {
   return (
-    <section className="flex justify-between flex-nowrap gap-4 w-full px-4 max-md:flex-col-reverse">
-      <div className="md:w-2/4 max-md:w-full flex max-md:justify-center md:justify-end">
-        <img className="h-3/4" src={taxonomyImage} alt="taxonomy" />
-      </div>
+    <section className="flex justify-between flex-nowrap gap-4 w-full px-4 max-md:flex-col">
       <div className="md:w-2/4 max-md:w-full flex max-md:justify-center bg-yellow-300">
-      <img className="h-3/4" src={taxonomyImage} alt="taxonomy" />
+        <p className="font-bold text-4xl">فیلترها بر اساس طبقه بندی</p>
+        <p className="font-bold text-4xl">و فیلدهای سفارشی</p>
+      </div>
+      <div className="md:w-2/4 max-md:w-full flex max-md:justify-center md:justify-start">
+        <img className="max-lg:h-full lg:h-3/4" src={taxonomyImage} alt="taxonomy" />
       </div>
     </section>
   );

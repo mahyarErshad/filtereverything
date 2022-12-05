9f96ad9a3f091df5612d49d5b2767e2abe56f466
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-05 10:25:20 +0330

    Added 2 p tags

--- src/components/Body/TaxonomySection.jsx
+++ src/components/Body/TaxonomySection.jsx
@@ -4,9 +4,11 @@ import taxonomyImage from "../../assets/images/taxonomy.jpg";
 function TaxonomySection() {
   return (
     <section className="flex justify-between flex-nowrap gap-4 w-full px-4 max-md:flex-col">
-      <div className="md:w-2/4 max-md:w-full flex max-md:justify-center bg-yellow-300">
-        <p className="font-bold text-4xl">فیلترها بر اساس طبقه بندی</p>
-        <p className="font-bold text-4xl">و فیلدهای سفارشی</p>
+      <div className="md:w-2/4 max-md:w-full flex max-md:justify-center bg-yellow-300 md:pt-14">
+        <div className="flex flex-col items-end w-full pl-6">
+        <p className="font-bold text-3xl">فیلترها بر اساس طبقه بندی</p>
+        <p className="font-bold text-3xl">و فیلدهای سفارشی</p>
+        </div>
       </div>
       <div className="md:w-2/4 max-md:w-full flex max-md:justify-center md:justify-start">
         <img className="max-lg:h-full lg:h-3/4" src={taxonomyImage} alt="taxonomy" />

359e8410ba7c83ca73a64d357a2ef497a7ddab54
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-05 09:52:06 +0330

    Created the structure

--- src/components/Body/TaxonomySection.jsx
+++ src/components/Body/TaxonomySection.jsx
@@ -2,7 +2,12 @@ import React, { memo } from 'react'
 
 function TaxonomySection() {
   return (
-    <div>TaxonomySection</div>
+    <section className='flex gap-6 flex-wrap w-full'>
+        <div className='md:w-2/4 max-md:w-full'>
+            
+        </div>
+        <div className='md:w-2/4 max-md:w-full'></div>
+    </section>
   )
 }
 

e9454c284ef8cd656e091a88352b29d3b390ae65
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-05 09:54:31 +0330

    Added the image

--- src/components/Body/TaxonomySection.jsx
+++ src/components/Body/TaxonomySection.jsx
@@ -1,15 +1,15 @@
-import React, { memo } from 'react'
-import taxonomyImage from "../../assets/images/taxonomy.jpg"
+import React, { memo } from "react";
+import taxonomyImage from "../../assets/images/taxonomy.jpg";
 
 function TaxonomySection() {
   return (
-    <section className='flex gap-6 flex-wrap w-full'>
-        <div className='md:w-2/4 max-md:w-full'>
-            
-        </div>
-        <div className='md:w-2/4 max-md:w-full'></div>
+    <section className="flex gap-6 flex-wrap w-full">
+      <div className="md:w-2/4 max-md:w-full">
+        <img src={taxonomyImage} alt="taxonomy" />
+      </div>
+      <div className="md:w-2/4 max-md:w-full"></div>
     </section>
-  )
+  );
 }
 
-export default memo(TaxonomySection)
+export default memo(TaxonomySection);

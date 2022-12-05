32a2d1ef40a62aa665e66d9d7fe652a06c029860
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-05 09:54:58 +0330

    Import MainBody

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { memo } from "react";
+import MainBody from "./components/Body/MainBody";
 import Header from "./components/Header/Header";
 
 function App() {
   return <>
   <Header />
+  <MainBody />
   </>;
 }
 

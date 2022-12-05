a10c5602e8370ccd1b7e671ff663b8f202e20f6e
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-05 10:09:32 +0330

    Image is responsive

--- src/components/Body/TaxonomySection.jsx
+++ src/components/Body/TaxonomySection.jsx
@@ -3,11 +3,11 @@ import taxonomyImage from "../../assets/images/taxonomy.jpg";
 
 function TaxonomySection() {
   return (
-    <section className="flex flex-wrap justify-between w-full">
-      <div className="md:w-[49%] max-md:w-full pr-12 bg-red-400">
+    <section className="flex justify-between flex-nowrap gap-4 w-full px-4 max-md:flex-col-reverse">
+      <div className="md:w-2/4 max-md:w-full flex max-md:justify-center md:justify-end">
         <img className="h-3/4" src={taxonomyImage} alt="taxonomy" />
       </div>
-      <div className="md:w-[49%] max-md:w-full bg-yellow-300">
+      <div className="md:w-2/4 max-md:w-full flex max-md:justify-center bg-yellow-300">
       <img className="h-3/4" src={taxonomyImage} alt="taxonomy" />
       </div>
     </section>

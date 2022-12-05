072f7b709e8d0ac6fd78830da378558d72fa225c
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-05 09:43:49 +0330

    Giving a py-2 to the button

--- src/components/Header/HeaderTextSection.jsx
+++ src/components/Header/HeaderTextSection.jsx
@@ -23,7 +23,7 @@ function HeaderTextSection() {
         <p className="text-white mt-6">غول فیلتر - افزونه فیلتر محصولات وردپرس و ووکامرس که به شما اجازه پیاده سازی هر نوع فیلتری را می دهد.</p>
       </div>
       <div className="flex justify-between items-center flex-wrap mt-6 max-md:w-[16.5rem] md:w-[25rem] gap-4">
-        <Button title="دانلود رایگان" className="download-for-free-button" backgroundColor="bg-transparent" textColor="text-white" />
+        <Button title="دانلود رایگان" className="download-for-free-button py-2" backgroundColor="bg-transparent" textColor="text-white" />
         <Button title="خرید نسخه حرفه ای" className="hover:bg-[#18caca] font-bold py-[1.05rem] max-md:px-11" />
       </div>
     </div>

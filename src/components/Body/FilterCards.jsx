472deb03c82f5a0be4d35404899db09ed0288d8c
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-06 08:37:14 +0330

    P tags are responsive

--- src/components/Body/FilterCards.jsx
+++ src/components/Body/FilterCards.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from "react";
 
 function FilterCards() {
   return (
-    <section>
-    <div>
-        <p>هر فیلتری ایجاد کنید</p>
-        <p>با هر پارامتری!</p>
-    </div>
+    <section className="flex flex-col gap-4 w-full bg-[#F5F4F0] md:py-8 max-md:py-4">
+      <div className="flex flex-col gap-4 max-md:items-center md:px-[15%] max-md:px-4 ">
+        <p className=" font-bold max-md:text-2xl md:text-3xl">هر فیلتری ایجاد کنید</p>
+        <p className="md:mr-36 font-bold max-md:text-2xl md:text-3xl">با هر پارامتری!</p>
+      </div>
     </section>
-  )
+  );
 }
 
-export default FilterCards
+export default FilterCards;

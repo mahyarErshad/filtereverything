cec3f9e58d003bf4f755e10d455e78086b01e17e
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-06 09:34:58 +0330

    Spelling has corrected

--- src/components/Body/TaxonomySection.jsx
+++ src/components/Body/TaxonomySection.jsx
@@ -6,7 +6,7 @@ function TaxonomySection() {
     <section className="flex flex-nowrap w-full px-4 lg:px-[10%] max-md:flex-col max-sm:mt-4">
       <div className="md:w-2/4 max-md:w-full flex flex-col max-lg:px-4 lg:px-[8%] justify-between md:pb-[7%] md:pt-[8%]">
         <div className="flex flex-col w-full gap-4 max-md:justify-center max-md:items-center">
-          <p className="font-bold max-lg:text-xl lg:text-3xl break-keep">فیلترها بر اساس طبقه بندی</p>
+          <p className="font-bold max-lg:text-xl lg:text-3xl break-keep">فیلترهای بر اساس طبقه بندی</p>
           <p className="font-bold max-lg:text-xl lg:text-3xl">و فیلدهای سفارشی</p>
           <p className="text-base text-[#676767] font-medium mt-8">این ویژگی 99 درصد نیازهای فیلترینگ احتمالی را پوشش می دهد.</p>
         </div>

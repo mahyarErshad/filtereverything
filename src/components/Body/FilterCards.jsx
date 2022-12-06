6466562674bd5a6d360ebad547d71573de313998
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-06 08:41:24 +0330

    Added the third p tag

--- src/components/Body/FilterCards.jsx
+++ src/components/Body/FilterCards.jsx
@@ -6,6 +6,7 @@ function FilterCards() {
       <div className="flex flex-col gap-4 max-md:items-center md:px-[15%] max-md:px-4 ">
         <p className=" font-bold max-md:text-2xl md:text-3xl">هر فیلتری ایجاد کنید</p>
         <p className="md:mr-36 font-bold max-md:text-2xl md:text-3xl">با هر پارامتری!</p>
+        <p className="md:text-lg max-md:text-base font-medium text-[#222]">برای هر نوع پست که دارای آرشیو است.</p>
       </div>
     </section>
   );

d5e3dcc9efd516eb462b126c68ce2660ea4738b5
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-06 09:23:01 +0330

    Changed the align self center to start

--- src/components/Body/TaxonomySection.jsx
+++ src/components/Body/TaxonomySection.jsx
@@ -7,7 +7,7 @@ function TaxonomySection() {
       <div className="md:w-2/4 max-md:w-full flex flex-col max-lg:px-4 lg:px-[8%] justify-between md:pb-[7%] md:pt-[8%]">
         <div className="flex flex-col w-full gap-4 max-md:justify-center max-md:items-center">
           <p className="font-bold max-lg:text-xl lg:text-3xl break-keep">فیلترها بر اساس طبقه بندی</p>
-          <p className="font-bold max-lg:text-xl lg:text-3xl max-lg:self-center lg:self-center">و فیلدهای سفارشی</p>
+          <p className="font-bold max-lg:text-xl lg:text-3xl">و فیلدهای سفارشی</p>
           <p className="text-base text-[#676767] font-medium mt-8">این ویژگی 99 درصد نیازهای فیلترینگ احتمالی را پوشش می دهد.</p>
         </div>
           <span className="max-sm:hidden bg-purple rounded-sm p-3 w-fit text-white max-md:mt-4">پس شما میتوانید</span>

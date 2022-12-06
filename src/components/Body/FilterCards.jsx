c44cfbeb96455439b87b36547d3cda61357c3462
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-06 08:25:12 +0330

    Added 2 p tags

--- src/components/Body/FilterCards.jsx
+++ src/components/Body/FilterCards.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 function FilterCards() {
   return (
     <section>
-    
+    <div>
+        <p>هر فیلتری ایجاد کنید</p>
+        <p>با هر پارامتری!</p>
+    </div>
     </section>
   )
 }

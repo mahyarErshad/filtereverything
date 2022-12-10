3e6c8ea1c721a7d725892972f31e798845d557a6
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-10 09:04:26 +0330

    Import FilterSets

--- src/components/Body/MainBody.jsx
+++ src/components/Body/MainBody.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import FilterCards from "./FilterCards";
 import TaxonomySection from "./TaxonomySection";
+import FilterSets from "./FilterSets";
 
 function MainBody() {
   return (
     <>
       <TaxonomySection />
       <FilterCards />
+      <FilterSets />
     </>
   );
 }

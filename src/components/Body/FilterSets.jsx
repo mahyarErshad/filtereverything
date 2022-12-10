f7b4b753999238c922cfb0166ec9ea8592c1cb03
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-10 09:39:26 +0330

    FilterSets section has been completed

--- src/components/Body/FilterSets.jsx
+++ src/components/Body/FilterSets.jsx
@@ -1,19 +1,23 @@
-import React from "react";
-import filterSetsImage from "../../assets/images/filter-sets.jpg"
-
+import React, { memo } from "react";
+import filterSetsImage from "../../assets/images/filter-sets.jpg";
 
 function FilterSets() {
   return (
-    <section className="flex flex-wrap w-full md:py-8 max-md:py-4 px-2">
-      <div className="flex flex-col md:gap-2 max-md:items-center md:items-end md:pr-[15%] pl-2 max-md:px-4 py-[5%] bg-red-500 max-md:w-full md:w-2/4">
-        <h5>مجموعه فیلترهای <span className="text-purple">شخصی</span> ایجاد کنید</h5>
-        <h4>برای هر صفحه سایت وردپرس خودتان</h4>
+    <section className="flex flex-nowrap w-full px-4 lg:px-[10%] max-md:flex-col max-sm:mt-4">
+      <div className="md:w-2/4 max-md:w-full flex flex-col max-lg:px-4 lg:pr-[8%] md:items-end justify-between md:pb-[7%] md:pt-[8%]">
+        <div className="flex flex-col w-full gap-4 max-md:justify-center max-md:items-center md:items-end">
+          <p className="font-bold max-lg:text-xl lg:text-3xl break-keep">
+            مجموعه فیلترهای <span className="text-purple">شخصی</span> ایجاد کنید
+          </p>
+          <p className="text-base text-[#676767] font-medium mt-8">برای هر صفحه سایت وردپرس شما.</p>
+        </div>
+        <span className="max-sm:hidden bg-purple rounded-sm p-3 w-fit text-white max-md:mt-4">همچنین</span>
       </div>
-      <div className="md:w-2/4 max-md:w-full flex max-md:justify-center p-[5%] pr-2 bg-yellow-300">
-      <img src={filterSetsImage} alt="filter sets" />
+      <div className="md:w-2/4 max-md:w-full flex max-md:justify-center p-[5%]">
+        <img className="h-auto max-w-full" src={filterSetsImage} alt="taxonomy" />
       </div>
     </section>
   );
 }
 
-export default FilterSets;
+export default memo(FilterSets);

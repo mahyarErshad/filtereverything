4e9f0ff1f83ef201c2ad0e1b09d9e47e6645df0a
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-10 09:08:26 +0330

    Giving needed classnames

--- src/components/Body/FilterSets.jsx
+++ src/components/Body/FilterSets.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function FilterSets() {
   return (
     <section className='w-full px-[10%] py-6 flex flex-wrap'>
-    <div></div>
-    <div></div>
+    <div className='flex max-md:w-full md:w-2/4'></div>
+    <div className='flex max-md:w-full md:w-2/4'></div>
     </section>
   )
 }

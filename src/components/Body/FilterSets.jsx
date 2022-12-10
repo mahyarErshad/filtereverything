0dfdcf6f88e66861d90c3b5361b8b22621c75a0a
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-10 09:10:54 +0330

    Adding a h5

--- src/components/Body/FilterSets.jsx
+++ src/components/Body/FilterSets.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from "react";
 
 function FilterSets() {
   return (
-    <section className='w-full px-[10%] py-6 flex flex-wrap'>
-    <div className='flex max-md:w-full md:w-2/4'></div>
-    <div className='flex max-md:w-full md:w-2/4'></div>
+    <section className="w-full px-[10%] py-6 flex flex-wrap">
+      <div className="flex max-md:w-full md:w-2/4">
+        <h5>مجموعه فیلترهای شخصی ایجاد کنید.</h5>
+      </div>
+      <div className="flex max-md:w-full md:w-2/4"></div>
     </section>
-  )
+  );
 }
 
-export default FilterSets
+export default FilterSets;

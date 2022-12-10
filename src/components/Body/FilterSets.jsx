5f6cec08b25204de787062c7eb12e0c4b7a42908
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-10 09:19:25 +0330

    Texts have been added without any styles

--- src/components/Body/FilterSets.jsx
+++ src/components/Body/FilterSets.jsx
@@ -2,11 +2,12 @@ import React from "react";
 
 function FilterSets() {
   return (
-    <section className="w-full px-[10%] py-6 flex flex-wrap">
-      <div className="flex max-md:w-full md:w-2/4">
-        <h5>مجموعه فیلترهای شخصی ایجاد کنید.</h5>
+    <section className="flex flex-wrap w-full md:py-8 max-md:py-4 px-2">
+      <div className="flex flex-col md:gap-2 max-md:items-center md:items-end md:pr-[15%] pl-2 max-md:px-4 py-[5%] bg-red-500 max-md:w-full md:w-2/4">
+        <h5>مجموعه فیلترهای <span className="text-purple">شخصی</span> ایجاد کنید</h5>
+        <h4>برای هر صفحه سایت وردپرس خودتان</h4>
       </div>
-      <div className="flex max-md:w-full md:w-2/4"></div>
+      <div className="flex max-md:w-full md:w-2/4 bg-yellow-300"></div>
     </section>
   );
 }

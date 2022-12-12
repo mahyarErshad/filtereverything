f817eb0d4f3636951830d6acd6b974703991c1a4
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-12 09:38:48 +0330

    MobileView is done

--- src/components/Body/MobileView.jsx
+++ src/components/Body/MobileView.jsx
@@ -5,10 +5,10 @@ function MobileView() {
   return (
     <section className="flex flex-nowrap w-full px-4 lg:px-[10%] max-md:flex-col max-sm:mt-4">
       <div className="md:w-2/4 max-md:w-full flex flex-col max-lg:px-4 lg:pr-[8%] md:items-end justify-between md:pb-[7%] md:pt-[8%]">
-        <div className="flex flex-col w-full gap-4 max-md:justify-center max-md:items-center md:items-end">
-          <p className="font-bold max-lg:text-xl lg:text-3xl break-keep self-center">آماده استفاده بر روی</p>
-          <p className="font-bold max-lg:text-xl lg:text-3xl">دستگاه های موبایل</p>
-          <p className="text-base text-[#676767] font-medium mt-8 max-md:self-start md:self-center">ویجت مخصوص زیرین به کاربران اجازه می دهد محصولات مورد نیازشان را تنها در چند کلیک پیدا کنند.</p>
+        <div className="flex flex-col w-full gap-4 max-md:justify-center max-md:items-center md:items-start">
+          <p className="font-bold max-lg:text-xl lg:text-3xl break-keep">آماده استفاده بر روی</p>
+          <p className="font-bold max-lg:text-xl lg:text-3xl self-center">دستگاه های موبایل</p>
+          <p className="text-base text-[#676767] font-medium mt-8">ویجت مخصوص زیرین به کاربران اجازه می دهد محصولات مورد نیازشان را تنها در چند کلیک پیدا کنند.</p>
         </div>
       </div>
       <div className="md:w-2/4 max-md:w-full flex max-md:justify-center p-[5%]">

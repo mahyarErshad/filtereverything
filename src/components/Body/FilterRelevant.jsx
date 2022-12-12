e93eeedd55aad39b39dea9d698f1065d1d621f73
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-12 08:48:24 +0330

    FilterRelevant is responsive

--- src/components/Body/FilterRelevant.jsx
+++ src/components/Body/FilterRelevant.jsx
@@ -4,7 +4,7 @@ import filterRelevant from "../../assets/images/filterRelevant.jpg";
 function FilterRelevant() {
   return (
     <section className="flex flex-col md:gap-14 max-md:gap-6 w-full bg-[#445DC4] md:py-8 max-md:py-4 px-2 items-center">
-      <p className="md:text-4xl max-md:text-2xl font-bold text-white mt-4">فیلتر محصولات، پست ها فقط بر اساس پارامترهای مرتبط!</p>
+      <p className="md:text-4xl sm:text-2xl max-sm:text-lg font-bold text-white mt-4">فیلتر محصولات، پست ها فقط بر اساس پارامترهای مرتبط!</p>
       <img src={filterRelevant} alt="card filter" />
     </section>
   );

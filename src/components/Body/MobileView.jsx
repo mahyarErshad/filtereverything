e1cf4f34c281070bf178bf1e4eb32d72eebe51ba
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-12 09:24:47 +0330

    Copy the rest of the code

--- src/components/Body/MobileView.jsx
+++ src/components/Body/MobileView.jsx
@@ -6,12 +6,11 @@ function MobileView() {
     <section className="flex flex-nowrap w-full px-4 lg:px-[10%] max-md:flex-col max-sm:mt-4">
       <div className="md:w-2/4 max-md:w-full flex flex-col max-lg:px-4 lg:pr-[8%] md:items-end justify-between md:pb-[7%] md:pt-[8%]">
         <div className="flex flex-col w-full gap-4 max-md:justify-center max-md:items-center md:items-end">
-          <p className="font-bold max-lg:text-xl lg:text-3xl break-keep">
-            مجموعه فیلترهای <span className="text-purple">شخصی</span> ایجاد کنید
-          </p>
-          <p className="text-base text-[#676767] font-medium mt-8">برای هر صفحه سایت وردپرس شما.</p>
+          <p className="font-bold max-lg:text-xl lg:text-3xl break-keep">فیلترهای بر اساس طبقه بندی</p>
+          <p className="font-bold max-lg:text-xl lg:text-3xl">و فیلدهای سفارشی</p>
+          <p className="text-base text-[#676767] font-medium mt-8">این ویژگی 99 درصد نیازهای فیلترینگ احتمالی را پوشش می دهد.</p>
         </div>
-        <span className="max-sm:hidden bg-purple rounded-sm p-3 w-fit text-white max-md:mt-4">همچنین</span>
+        <span className="max-sm:hidden bg-purple rounded-sm p-3 w-fit text-white max-md:mt-4">پس شما میتوانید</span>
       </div>
       <div className="md:w-2/4 max-md:w-full flex max-md:justify-center p-[5%]">
         <img className="h-auto max-w-full" src={mobileView} alt="taxonomy" />

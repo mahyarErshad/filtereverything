1922f7c2167126a98c8a37c71ef271c95999d83c
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-12 09:29:27 +0330

    Texts have been changed

--- src/components/Body/MobileView.jsx
+++ src/components/Body/MobileView.jsx
@@ -6,11 +6,10 @@ function MobileView() {
     <section className="flex flex-nowrap w-full px-4 lg:px-[10%] max-md:flex-col max-sm:mt-4">
       <div className="md:w-2/4 max-md:w-full flex flex-col max-lg:px-4 lg:pr-[8%] md:items-end justify-between md:pb-[7%] md:pt-[8%]">
         <div className="flex flex-col w-full gap-4 max-md:justify-center max-md:items-center md:items-end">
-          <p className="font-bold max-lg:text-xl lg:text-3xl break-keep">فیلترهای بر اساس طبقه بندی</p>
-          <p className="font-bold max-lg:text-xl lg:text-3xl">و فیلدهای سفارشی</p>
-          <p className="text-base text-[#676767] font-medium mt-8">این ویژگی 99 درصد نیازهای فیلترینگ احتمالی را پوشش می دهد.</p>
+          <p className="font-bold max-lg:text-xl lg:text-3xl break-keep">آماده استفاده بر روی</p>
+          <p className="font-bold max-lg:text-xl lg:text-3xl">دستگاه های موبایل</p>
+          <p className="text-base text-[#676767] font-medium mt-8">ویجت مخصوص زیرین به کاربران اجازه می دهد محصولات مورد نیازشان را تنها در چند کلیک پیدا کنند.</p>
         </div>
-        <span className="max-sm:hidden bg-purple rounded-sm p-3 w-fit text-white max-md:mt-4">پس شما میتوانید</span>
       </div>
       <div className="md:w-2/4 max-md:w-full flex max-md:justify-center p-[5%]">
         <img className="h-auto max-w-full" src={mobileView} alt="taxonomy" />

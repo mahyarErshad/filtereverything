b6b465c73a1144c2661368f24933da767b6e9314
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-13 10:12:25 +0330

    Adding a new section

--- src/components/Body/CleanURLs.jsx
+++ src/components/Body/CleanURLs.jsx
@@ -13,11 +13,17 @@ function CleanURLs() {
           <p>این باعث می شود که آنها برای ایندکس شدن توسط موتورهای جستجو در دسترس باشند.</p>
         </p>
       </div>
-      <img className="max-lg:w-full lg:w-3/4 self-center md:mb-32 max-md:mb-6" src={clean_URLs} alt="card filter" />
-      <div className="flex flex-col md:gap-2 max-lg:items-center lg:px-[25%] max-md:px-4 mb-6">
+      <img className="max-lg:w-full lg:w-3/4 self-center md:mb-32 max-md:mb-6" src={clean_URLs} alt="Clean_URLs" />
+      <div className="flex flex-col md:gap-2 max-lg:items-center lg:pr-[25%] lg:pl-[10%] max-md:px-4 mb-6">
         <p className="flex items-center font-bold max-sm:text-xl max-md:text-2xl md:text-3xl text-[#222] gap-3">
           ایجاد قوانین سئو برای<span className="text-purple">/آنها</span>
         </p>
+        <div className="flex flex-wrap justify-center w-full">
+          <div className="flex max-md:w-full md:w-2/4"></div>
+          <div className="flex max-md:w-full md:w-2/4">
+            <img src={SEO_Rules} alt="SEO_Rules" />
+          </div>
+        </div>
       </div>
     </section>
   );

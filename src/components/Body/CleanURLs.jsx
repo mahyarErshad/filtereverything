9b8cf570a21d470275e344eb06accf726beeabea
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-13 09:26:13 +0330

    Text purple added for links

--- src/components/Body/CleanURLs.jsx
+++ src/components/Body/CleanURLs.jsx
@@ -6,7 +6,7 @@ function CleanURLs() {
     <section className="flex flex-col gap-4 w-full bg-[#F4F9FA] md:py-36 max-md:py-4 px-2">
       <div className="flex flex-col md:gap-2 max-md:items-center md:px-[25%] max-md:px-4 ">
         <p className="flex items-center font-bold max-sm:text-xl max-md:text-2xl md:text-3xl text-[#222]">
-          URL های تمیز برای <span className="text-[#445dc8]">/فیلتر/نتایج/صفحات</span>
+          URL های تمیز برای <span className="text-purple">/فیلتر/نتایج/صفحات</span>
         </p>
         <p className="md:text-lg max-md:text-base font-medium text-[#222] mt-4">
           <p>این باعث می شود که آنها برای ایندکس شدن توسط موتورهای جستجو در دسترس باشند.</p>

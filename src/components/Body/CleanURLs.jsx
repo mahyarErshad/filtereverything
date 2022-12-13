95f646363b7af3ea1d96caca5c3e489afa5dfb66
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-13 09:53:00 +0330

    Adding another section

--- src/components/Body/CleanURLs.jsx
+++ src/components/Body/CleanURLs.jsx
@@ -4,7 +4,7 @@ import clean_URLs from "../../assets/images/Clean_URLs.jpg";
 function CleanURLs() {
   return (
     <section className="flex flex-col gap-4 w-full bg-[#F4F9FA] md:py-36 max-md:py-4 px-2">
-      <div className="flex flex-col md:gap-2 max-md:items-center md:px-[25%] max-md:px-4 mb-6">
+      <div className="flex flex-col md:gap-2 max-lg:items-center lg:px-[25%] max-md:px-4 mb-6">
         <p className="flex items-center font-bold max-sm:text-xl max-md:text-2xl md:text-3xl text-[#222] gap-3">
           URL های تمیز برای <span className="text-purple">/فیلتر/نتایج/صفحات</span>
         </p>
@@ -12,7 +12,7 @@ function CleanURLs() {
           <p>این باعث می شود که آنها برای ایندکس شدن توسط موتورهای جستجو در دسترس باشند.</p>
         </p>
       </div>
-      <img className="max-md:w-full md:w-3/4 self-center" src={clean_URLs} alt="card filter" />
+      <img className="max-lg:w-full lg:w-3/4 self-center md:mb-32 max-md:mb-6" src={clean_URLs} alt="card filter" />
     </section>
   );
 }

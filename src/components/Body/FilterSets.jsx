6575371d71dc87906e9b772fc96772295bb19c2c
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-13 11:00:16 +0330

    Set py on small screens

--- src/components/Body/FilterSets.jsx
+++ src/components/Body/FilterSets.jsx
@@ -4,7 +4,7 @@ import filterSetsImage from "../../assets/images/filter-sets.jpg";
 function FilterSets() {
   return (
     <section className="flex flex-nowrap w-full px-4 lg:px-[10%] max-md:flex-col max-sm:mt-4">
-      <div className="md:w-2/4 max-md:w-full flex flex-col max-lg:px-4 lg:pr-[8%] md:items-end justify-between md:pb-[7%] md:pt-[8%]">
+      <div className="md:w-2/4 max-md:w-full flex flex-col max-lg:px-4 lg:pr-[8%] md:items-end justify-between max-md:py-4 md:pb-[7%] md:pt-[8%]">
         <div className="flex flex-col w-full gap-4 max-md:justify-center max-md:items-center md:items-end">
           <p className="font-bold max-lg:text-xl lg:text-3xl break-keep">
             مجموعه فیلترهای <span className="text-purple">شخصی</span> ایجاد کنید

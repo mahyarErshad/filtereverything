6646116604495ecea73a41e91d058d455006e46b
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-13 09:09:20 +0330

    Copied the codes

--- src/components/Body/CleanURLs.jsx
+++ src/components/Body/CleanURLs.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
 function CleanURLs() {
   return (
-    <div>CleanURLs</div>
-  )
+    <section className="flex flex-col gap-4 w-full bg-[#F5F4F0] md:py-8 max-md:py-4 px-2">
+      <div className="flex flex-col md:gap-2 max-md:items-center md:px-[15%] max-md:px-4 ">
+        <p className=" font-thin max-md:text-2xl md:text-3xl text-[#222]">
+          <span className="text-purple">هر</span> فیلتری ایجاد کنید
+        </p>
+        <p className="font-bold text-[2.2rem]">
+          با <span className="text-purple">هر</span> پارامتری!
+        </p>
+        <p className="md:text-lg max-md:text-base font-medium text-[#222] mt-4">
+          برای <span className="text-purple">هر</span> نوع پست که دارای آرشیو است.
+        </p>
+      </div>
+      <img src={cardFilterImage} alt="card filter" />
+    </section>
+  );
 }
 
-export default CleanURLs
+export default CleanURLs;

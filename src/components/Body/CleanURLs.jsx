9f341c6669b0b546fb5f345b2dc3844b764a38b7
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-13 09:10:19 +0330

    Changed the padding of y

--- src/components/Body/CleanURLs.jsx
+++ src/components/Body/CleanURLs.jsx
@@ -1,8 +1,9 @@
 import React, { memo } from "react";
+import cardFilterImage from "../../assets/images/filterCards.jpg";
 
 function CleanURLs() {
   return (
-    <section className="flex flex-col gap-4 w-full bg-[#F5F4F0] md:py-8 max-md:py-4 px-2">
+    <section className="flex flex-col gap-4 w-full bg-[#F5F4F0] md:py-10 max-md:py-4 px-2">
       <div className="flex flex-col md:gap-2 max-md:items-center md:px-[15%] max-md:px-4 ">
         <p className=" font-thin max-md:text-2xl md:text-3xl text-[#222]">
           <span className="text-purple">هر</span> فیلتری ایجاد کنید

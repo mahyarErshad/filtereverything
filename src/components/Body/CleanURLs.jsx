ca356c55279aa369b32d4587cb80982bf806c701
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-13 09:29:03 +0330

    Texts are done

--- src/components/Body/CleanURLs.jsx
+++ src/components/Body/CleanURLs.jsx
@@ -5,7 +5,7 @@ function CleanURLs() {
   return (
     <section className="flex flex-col gap-4 w-full bg-[#F4F9FA] md:py-36 max-md:py-4 px-2">
       <div className="flex flex-col md:gap-2 max-md:items-center md:px-[25%] max-md:px-4 ">
-        <p className="flex items-center font-bold max-sm:text-xl max-md:text-2xl md:text-3xl text-[#222] gap-4">
+        <p className="flex items-center font-bold max-sm:text-xl max-md:text-2xl md:text-3xl text-[#222] gap-3">
           URL های تمیز برای <span className="text-purple">/فیلتر/نتایج/صفحات</span>
         </p>
         <p className="md:text-lg max-md:text-base font-medium text-[#222] mt-4">

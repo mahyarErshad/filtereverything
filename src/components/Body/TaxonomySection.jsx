fb40dc33c625e1f03600b580f2c117f67a3e7e5e
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-13 10:56:50 +0330

    Set py on md screens

--- src/components/Body/TaxonomySection.jsx
+++ src/components/Body/TaxonomySection.jsx
@@ -4,7 +4,7 @@ import taxonomyImage from "../../assets/images/taxonomy.jpg";
 function TaxonomySection() {
   return (
     <section className="flex flex-nowrap w-full px-4 lg:px-[10%] max-md:flex-col max-sm:mt-4">
-      <div className="md:w-2/4 max-md:w-full flex flex-col max-lg:px-4 lg:pr-[8%] md:items-end justify-between md:pb-[7%] md:pt-[8%]">
+      <div className="md:w-2/4 max-md:w-full flex flex-col max-lg:px-4 lg:pr-[8%] md:items-end justify-between max-md:py-4 md:pb-[7%] md:pt-[8%]">
         <div className="flex flex-col w-full gap-4 max-md:justify-center max-md:items-center md:items-end">
           <p className="font-bold max-lg:text-xl lg:text-3xl break-keep self-center">فیلترهای بر اساس طبقه بندی</p>
           <p className="font-bold max-lg:text-xl lg:text-3xl">و فیلدهای سفارشی</p>

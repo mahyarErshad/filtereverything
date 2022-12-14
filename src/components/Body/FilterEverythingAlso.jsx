05acbf03c71265aafa58e2263c33872021011fb7
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-14 11:09:18 +0330

    Card wrapper has a display flex

--- src/components/Body/FilterEverythingAlso.jsx
+++ src/components/Body/FilterEverythingAlso.jsx
@@ -9,7 +9,7 @@ function FilterEverythingAlso() {
         <h5 className="lg:text-4xl md:text-3xl max-md:text-2xl font-bold">
           غول فیلتر <span className="text-purple">همچنین</span>
         </h5>
-        <div className="max-md:mt-8 md:mt-12 lg:mt-20">
+        <div className="max-md:mt-8 md:mt-12 lg:mt-20 flex flex-wrap w-full">
             <FilterEverythingAlsoCards icon={puzzleIcon} title="سازگار با تم شما" description="به طور خودکار با اکثر تم ها کار می کند." />
         </div>
       </div>

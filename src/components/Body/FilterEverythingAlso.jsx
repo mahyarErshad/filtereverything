1e19267c9743b86e7ff1dcbcbc882f37df6f0a3d
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-14 08:17:58 +0330

    Copied the section and the wrapper

--- src/components/Body/FilterEverythingAlso.jsx
+++ src/components/Body/FilterEverythingAlso.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function FilterEverythingAlso() {
   return (
-    <div>FilterEverythingAlso</div>
-  )
+    <section className="flex flex-col gap-4 w-full bg-[#F4F9FA] md:pt-36 max-md:pt-8 px-2 max-md:pb-4 md: pb-8">
+      <div className="flex flex-col md:gap-2 max-lg:items-center lg:px-[25%] max-md:px-4 mb-6"></div>
+    </section>
+  );
 }
 
-export default FilterEverythingAlso
+export default FilterEverythingAlso;

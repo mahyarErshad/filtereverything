8d3c813fd12d77e88342f863b901345967fa3e36
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-14 10:45:58 +0330

    Initialized the skeleton

--- src/components/Utils/Button/FilterEverythingAlsoCards/FilterEverythingAlsoCards.jsx
+++ src/components/Utils/Button/FilterEverythingAlsoCards/FilterEverythingAlsoCards.jsx
@@ -1,1 +1,15 @@
-rfce
+import React from 'react'
+
+function FilterEverythingAlsoCards(props) {
+  return (
+    <div>
+    <div>
+        <div></div>
+        <div></div>
+    </div>
+    <div></div>
+    </div>
+  )
+}
+
+export default FilterEverythingAlsoCards

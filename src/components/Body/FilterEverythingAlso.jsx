c5e2bfd81a3a8812c36cdcd759238e02fc090d7a
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-14 08:18:27 +0330

    Background color is white

--- src/components/Body/FilterEverythingAlso.jsx
+++ src/components/Body/FilterEverythingAlso.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 function FilterEverythingAlso() {
   return (
-    <section className="flex flex-col gap-4 w-full bg-[#F4F9FA] md:pt-36 max-md:pt-8 px-2 max-md:pb-4 md: pb-8">
-      <div className="flex flex-col md:gap-2 max-lg:items-center lg:px-[25%] max-md:px-4 mb-6"></div>
+    <section className="flex flex-col gap-4 w-full bg-white md:pt-36 max-md:pt-8 px-2 max-md:pb-4 md: pb-8">
+      <div className="flex flex-col md:gap-2 max-lg:items-center lg:px-[25%] max-md:px-4 mb-6">
+
+      </div>
     </section>
   );
 }

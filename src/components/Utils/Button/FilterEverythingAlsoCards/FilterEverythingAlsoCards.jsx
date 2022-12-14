ead029b4ace56ea67276eaae89d57031e67009b4
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-14 11:00:42 +0330

    Set the margin

--- src/components/Utils/Button/FilterEverythingAlsoCards/FilterEverythingAlsoCards.jsx
+++ src/components/Utils/Button/FilterEverythingAlsoCards/FilterEverythingAlsoCards.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function FilterEverythingAlsoCards(props) {
   return (
     <div className="flex flex-col lg:gap-8 max-lg:gap-4">
-      <div>
-        <img src={props.icon} alt={props.title} />
+      <div className="flex items-end gap-3">
+        <img className="max-w-[2.25rem]" src={props.icon} alt={props.title} />
         <h5>{props.title}</h5>
       </div>
       <p>{props.description}</p>

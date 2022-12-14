10ca5056873baa37fecbde6531f92c392e56d91b
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-14 11:02:28 +0330

    Title has been styled

--- src/components/Utils/Button/FilterEverythingAlsoCards/FilterEverythingAlsoCards.jsx
+++ src/components/Utils/Button/FilterEverythingAlsoCards/FilterEverythingAlsoCards.jsx
@@ -5,7 +5,7 @@ function FilterEverythingAlsoCards(props) {
     <div className="flex flex-col lg:gap-8 max-lg:gap-4">
       <div className="flex items-end gap-3">
         <img className="max-w-[2.25rem]" src={props.icon} alt={props.title} />
-        <h5>{props.title}</h5>
+        <h5 className="font-bold max-md:text-xl md:text-2xl">{props.title}</h5>
       </div>
       <p>{props.description}</p>
     </div>

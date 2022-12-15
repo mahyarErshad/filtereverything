2673831a90232bfca3dda606432d9c489b340771
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-15 09:01:56 +0330

    Created a memoized data

--- src/components/Body/FilterEverythingAlso.jsx
+++ src/components/Body/FilterEverythingAlso.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useMemo } from "react";
 import FilterEverythingAlsoCards from "../Utils/Button/FilterEverythingAlsoCards/FilterEverythingAlsoCards";
 import puzzleIcon from "../../assets/images/FilterEverythingAlsoCards-icons/puzzle.svg";
 
+
 function FilterEverythingAlso() {
+  const memoizedData = useMemo(() => {
+    return [
+      {
+      icon: puzzleIcon,
+      title: "سازگار با تم شما",
+      description: "به طور خودکار با اکثر تم ها کار می کند."
+    },
+  ]
+  }, [])
   return (
     <section className="flex flex-col gap-4 w-full bg-white md:pt-36 max-md:pt-8 px-2 max-md:pb-4 md: pb-8">
       <div className="flex flex-col md:gap-2 max-lg:items-center lg:px-[25%] max-md:px-4 mb-6">

14551799a82ba32d396053929a216cce00749a6a
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-15 09:36:25 +0330

    Cards are responsive

--- src/components/Utils/Button/FilterEverythingAlsoCards/FilterEverythingAlsoCards.jsx
+++ src/components/Utils/Button/FilterEverythingAlsoCards/FilterEverythingAlsoCards.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function FilterEverythingAlsoCards(props) {
   return (
-    <div className="flex flex-col lg:gap-8 max-lg:gap-4 max-md:w-full md:w-[30%]">
+    <div className="flex flex-col lg:gap-8 max-lg:gap-4 max-md:w-full md:w-[50%] lg:w-[30%]">
       <div className="flex items-end gap-3">
         <img className="max-w-[2.25rem]" src={props.icon} alt={props.title} />
         <h5 className="font-bold max-md:text-xl md:text-2xl">{props.title}</h5>

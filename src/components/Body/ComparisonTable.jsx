89eb485808c773461efd3212231887336ae161f3
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-17 09:22:33 +0330

    Items are center

--- src/components/Body/ComparisonTable.jsx
+++ src/components/Body/ComparisonTable.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function ComparisonTable() {
   return (
     <section className="flex flex-col gap-4 w-full bg-[#F4F9FA] md:pt-36 max-md:pt-8 px-2 max-md:pb-4 md: pb-8">
-      <div className="flex flex-col md:gap-2 max-lg:items-center lg:px-[25%] max-md:px-4 mb-6">
-      <h2 className=" max-md:text-2xl md:text-lg lg:text-3xl font-bold">غول فیلتر</h2>
+      <div className="flex flex-col md:gap-2 items-center lg:px-[25%] max-md:px-4 mb-6">
+      <h2 className=" max-md:text-2xl md:text-lg lg:text-3xl font-bold mb-4">غول فیلتر</h2>
       <h2 className=" max-md:text-2xl md:text-lg lg:text-3xl font-bold">تفاوت نسخه رایگان با نسخه حرفه ای</h2>
         <table>
 

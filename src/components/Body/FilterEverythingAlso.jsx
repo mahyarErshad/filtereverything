5cd28358ea0a071853eb7e74299a3c3b4b3b5619
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-17 10:36:27 +0330

    Copied the data from component

--- src/components/Body/FilterEverythingAlso.jsx
+++ src/components/Body/FilterEverythingAlso.jsx
@@ -8,66 +8,7 @@ import airplaneIcon from "../../assets/images/FilterEverythingAlsoCards-icons/ai
 import radioButtonIcon from "../../assets/images/FilterEverythingAlsoCards-icons/radioButton.svg";
 
 function FilterEverythingAlso() {
-  const memoizedData = useMemo(() => {
-    return [
-      {
-        id: 1,
-        icon: puzzleIcon,
-        title: "سازگار با تم شما",
-        description: "به طور خودکار با اکثر تم ها کار می کند.",
-      },
-      {
-        id: 2,
-        icon: ajaxIcon,
-        title: "پشتیبانی از AJAX",
-        description: (
-          <p>
-            پلاگین از بارگیری پست های فیلتر شده با{" "}
-            <a className="text-blue-500" target="_blank" rel="noreferrer" href="https://filtereverything.pro/resources/plugin-settings/#ajax">
-              AJAX
-            </a>{" "}
-            پشتیبانی می کند که بسیار سریعتر است.
-          </p>
-        ),
-      },
-      {
-        id: 3,
-        icon: developersFriendlyIcon,
-        title: "دوستدار توسعه دهندگان",
-        description: (
-          <p>
-            رفتار و ظاهر افزونه را از طریق{" "}
-            <a className="text-blue-500" target="_blank" rel="noreferrer" href="https://filtereverything.pro/resources/hooks/">
-              هوک ها
-            </a>{" "}
-            و ویژگی‌های اصلی{" "}
-            <a className="text-blue-500" target="_blank" rel="noreferrer" href="https://filtereverything.pro/resources/templates-overriding/">
-              الگو
-            </a>{" "}
-            اصلاح کنید.
-          </p>
-        ),
-      },
-      {
-        id: 4,
-        icon: wordpressIcon,
-        title: "از رابط کاربری بومی وردپرس استفاده کنید",
-        description: "برای دانستن نحوه کار با افزونه نیازی به حدس زدن یا یادگیری اسناد نیست. دارای رابط کاربری آشنا و بصری است.",
-      },
-      {
-        id: 5,
-        icon: airplaneIcon,
-        title: "سبک و سریع",
-        description: "به لطف کدهای خوبی که نوشته شده، مانند یک جت جنگنده پرواز می کند.",
-      },
-      {
-        id: 6,
-        icon: radioButtonIcon,
-        title: "با بهترین شیوه های فیلتر مطابقت دارد",
-        description: "الهام گرفته شده از محبوب ترین فروشگاه های اینترنتی در جهان.",
-      },
-    ];
-  }, []);
+  
   return (
     <section className="flex flex-col gap-4 w-full bg-white md:pt-36 max-md:pt-8 px-2 max-md:pb-4 md: pb-12">
       <div className="flex flex-col md:gap-2 max-lg:items-center lg:px-[25%] max-md:px-4 mb-6">
---
+++ src/lib/FilterEverythingAlsoData.js
@@ -0,0 +1,58 @@
+const FilterEverythingAlsoData = [
+  {
+    id: 1,
+    icon: puzzleIcon,
+    title: "سازگار با تم شما",
+    description: "به طور خودکار با اکثر تم ها کار می کند.",
+  },
+  {
+    id: 2,
+    icon: ajaxIcon,
+    title: "پشتیبانی از AJAX",
+    description: (
+      <p>
+        پلاگین از بارگیری پست های فیلتر شده با{" "}
+        <a className="text-blue-500" target="_blank" rel="noreferrer" href="https://filtereverything.pro/resources/plugin-settings/#ajax">
+          AJAX
+        </a>{" "}
+        پشتیبانی می کند که بسیار سریعتر است.
+      </p>
+    ),
+  },
+  {
+    id: 3,
+    icon: developersFriendlyIcon,
+    title: "دوستدار توسعه دهندگان",
+    description: (
+      <p>
+        رفتار و ظاهر افزونه را از طریق{" "}
+        <a className="text-blue-500" target="_blank" rel="noreferrer" href="https://filtereverything.pro/resources/hooks/">
+          هوک ها
+        </a>{" "}
+        و ویژگی‌های اصلی{" "}
+        <a className="text-blue-500" target="_blank" rel="noreferrer" href="https://filtereverything.pro/resources/templates-overriding/">
+          الگو
+        </a>{" "}
+        اصلاح کنید.
+      </p>
+    ),
+  },
+  {
+    id: 4,
+    icon: wordpressIcon,
+    title: "از رابط کاربری بومی وردپرس استفاده کنید",
+    description: "برای دانستن نحوه کار با افزونه نیازی به حدس زدن یا یادگیری اسناد نیست. دارای رابط کاربری آشنا و بصری است.",
+  },
+  {
+    id: 5,
+    icon: airplaneIcon,
+    title: "سبک و سریع",
+    description: "به لطف کدهای خوبی که نوشته شده، مانند یک جت جنگنده پرواز می کند.",
+  },
+  {
+    id: 6,
+    icon: radioButtonIcon,
+    title: "با بهترین شیوه های فیلتر مطابقت دارد",
+    description: "الهام گرفته شده از محبوب ترین فروشگاه های اینترنتی در جهان.",
+  },
+];

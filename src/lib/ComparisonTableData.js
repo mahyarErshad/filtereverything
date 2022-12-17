4e436401a6a7d28ecc0e0328b06f81ed12376079
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-17 10:50:27 +0330

    Skeleton has been created

--- src/lib/ComparisonTableData.js
+++ src/lib/ComparisonTableData.js
@@ -1,2 +1,8 @@
-const ComparisonTableData = [{}];
+const ComparisonTableData = [
+    {
+        title: "هر نوع پست (پست ها، محصولات و غیره) را فیلتر می کند",
+        pro: true,
+        free: true,
+},
+];
 export default ComparisonTableData;

e7c746e8a55722930798a6c32858417725caa981
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-17 08:51:29 +0330

    Changed pb to 12

--- src/components/Body/FilterEverythingAlso.jsx
+++ src/components/Body/FilterEverythingAlso.jsx
@@ -69,7 +69,7 @@ function FilterEverythingAlso() {
     ];
   }, []);
   return (
-    <section className="flex flex-col gap-4 w-full bg-white md:pt-36 max-md:pt-8 px-2 max-md:pb-4 md: pb-8">
+    <section className="flex flex-col gap-4 w-full bg-white md:pt-36 max-md:pt-8 px-2 max-md:pb-4 md: pb-12">
       <div className="flex flex-col md:gap-2 max-lg:items-center lg:px-[25%] max-md:px-4 mb-6">
         <h5 className="lg:text-4xl md:text-3xl max-md:text-2xl font-bold">
           غول فیلتر <span className="text-purple">همچنین</span>

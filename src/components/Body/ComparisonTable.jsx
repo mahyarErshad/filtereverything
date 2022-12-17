37eb572002cec07c387882386c0ccc36fe5efac8
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-17 11:25:48 +0330

    Changed the padding of td

--- src/components/Body/ComparisonTable.jsx
+++ src/components/Body/ComparisonTable.jsx
@@ -3,7 +3,7 @@ import ComparisonTableData from "../../lib/ComparisonTableData";
 
 function ComparisonTable() {
   return (
-    <section className="flex flex-col gap-4 w-full bg-white md:pt-36 max-md:pt-8 px-2 max-md:pb-4 md: pb-8">
+    <section className="flex flex-col gap-4 w-full bg-white md:pt-36 max-md:pt-8 px-2 max-md:pb-4 md: pb-8 border-t-2 border-[#e5e5e5]">
       <div className="flex flex-col md:gap-2 items-center lg:px-[25%] max-md:px-4 mb-6">
         <h2 className=" max-md:text-2xl md:text-lg lg:text-3xl font-bold mb-4">غول فیلتر</h2>
         <h2 className=" max-md:text-2xl md:text-lg lg:text-3xl font-bold max-lg:mb-4 lg:mb-8">تفاوت نسخه رایگان با نسخه حرفه ای</h2>

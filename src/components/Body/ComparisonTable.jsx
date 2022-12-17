e3a7dae87704c6196cc4ad1a66bef4faff653eb7
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-17 10:30:52 +0330

    Add the thead

--- src/components/Body/ComparisonTable.jsx
+++ src/components/Body/ComparisonTable.jsx
@@ -4,10 +4,14 @@ function ComparisonTable() {
   return (
     <section className="flex flex-col gap-4 w-full bg-[#F4F9FA] md:pt-36 max-md:pt-8 px-2 max-md:pb-4 md: pb-8">
       <div className="flex flex-col md:gap-2 items-center lg:px-[25%] max-md:px-4 mb-6">
-      <h2 className=" max-md:text-2xl md:text-lg lg:text-3xl font-bold mb-4">غول فیلتر</h2>
-      <h2 className=" max-md:text-2xl md:text-lg lg:text-3xl font-bold max-lg:mb-4 lg:mb-8">تفاوت نسخه رایگان با نسخه حرفه ای</h2>
+        <h2 className=" max-md:text-2xl md:text-lg lg:text-3xl font-bold mb-4">غول فیلتر</h2>
+        <h2 className=" max-md:text-2xl md:text-lg lg:text-3xl font-bold max-lg:mb-4 lg:mb-8">تفاوت نسخه رایگان با نسخه حرفه ای</h2>
         <table>
-
+          <thead>
+            <th></th>
+            <th>رایگان</th>
+            <th>حرفه ای</th>
+          </thead>
         </table>
       </div>
     </section>

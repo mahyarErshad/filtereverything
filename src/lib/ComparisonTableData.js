69cdf79a720b8d30facce7a7440b541b5b27ca36
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-17 10:59:11 +0330

    ComparisonTableData has all data it needs

--- src/lib/ComparisonTableData.js
+++ src/lib/ComparisonTableData.js
@@ -1,48 +1,83 @@
 const ComparisonTableData = [
-    {
-        title: "هر نوع پست (پست ها، محصولات و غیره) را فیلتر می کند",
-        pro: true,
-        free: true,
-},
-    {
-        title: "پشتیبانی از AJAX",
-        pro: true,
-        free: true,
-},
-    {
-        title: "شمارش، بازشماری پویا از نظر شرایط",
-        pro: true,
-        free: true,
-},
-    {
-        title: "ویجت مرتب سازی",
-        pro: true,
-        free: true,
-},
-    {
-        title: "پیشوندهای URL سفارشی برای فیلترها",
-        pro: true,
-        free: true,
-},
-    {
-        title: "فیلتر کردن کوئری های WP سفارشی",
-        pro: true,
-        free: false,
-},
-    {
-        title: "URL ها و پیوندهای ثابت مرتب",
-        pro: true,
-        free: false,
-},
-    {
-        title: "فیلتر هوشمند با تغییرات محصول Woo",
-        pro: true,
-        free: false,
-},
-    {
-        title: "فیلتر هوشمند با تغییرات محصول Woo",
-        pro: true,
-        free: false,
-},
+  {
+    title: "هر نوع پست (پست ها، محصولات و غیره) را فیلتر می کند",
+    pro: true,
+    free: true,
+  },
+  {
+    title: "پشتیبانی از AJAX",
+    pro: true,
+    free: true,
+  },
+  {
+    title: "شمارش، بازشماری پویا از نظر شرایط",
+    pro: true,
+    free: true,
+  },
+  {
+    title: "ویجت مرتب سازی",
+    pro: true,
+    free: true,
+  },
+  {
+    title: "پیشوندهای URL سفارشی برای فیلترها",
+    pro: true,
+    free: true,
+  },
+  {
+    title: "فیلتر کردن کوئری های WP سفارشی",
+    pro: true,
+    free: false,
+  },
+  {
+    title: "URL ها و پیوندهای ثابت مرتب",
+    pro: true,
+    free: false,
+  },
+  {
+    title: "فیلتر هوشمند با تغییرات محصول Woo",
+    pro: true,
+    free: false,
+  },
+  {
+    title: "مجموعه فیلترهای فردی برای هر صفحه و بایگانی",
+    pro: true,
+    free: false,
+  },
+  {
+    title: "سازگاری با صفحه سازها",
+    pro: true,
+    free: false,
+  },
+  {
+    title: "قوانین سئو، نمایه سازی توسط موتورهای جستجو",
+    pro: true,
+    free: false,
+  },
+  {
+    title: "ویجت فیلترهای پاپ آپ ویژه برای دستگاه های تلفن همراه",
+    pro: true,
+    free: false,
+  },
+  {
+    title: "محفظه AJAX جداگانه برای مجموعه های فیلتر",
+    pro: true,
+    free: false,
+  },
+  {
+    title: "اگر همه عبارت‌ها خالی هستند، فیلتر را پنهان کنید",
+    pro: true,
+    free: false,
+  },
+  {
+    title: "فیلتر رتبه بندی ستاره ها برای محصول",
+    pro: true,
+    free: false,
+  },
+  {
+    title: "پشتیبانی ویژه",
+    pro: true,
+    free: false,
+  },
 ];
 export default ComparisonTableData;

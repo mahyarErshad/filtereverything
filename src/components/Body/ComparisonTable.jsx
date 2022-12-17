89dc5b6cefcfea7a9be9a5982ba65fbdb5664215
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-17 09:17:47 +0330

    Add the wrapper

--- src/components/Body/ComparisonTable.jsx
+++ src/components/Body/ComparisonTable.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 function ComparisonTable() {
   return (
-    <div>ComparisonTable</div>
-  )
+    <section className="flex flex-col gap-4 w-full bg-[#F4F9FA] md:pt-36 max-md:pt-8 px-2 max-md:pb-4 md: pb-8">
+      <div className="flex flex-col md:gap-2 max-lg:items-center lg:px-[25%] max-md:px-4 mb-6">
+        <table>
+            
+        </table>
+      </div>
+    </section>
+  );
 }
 
-export default ComparisonTable
+export default ComparisonTable;

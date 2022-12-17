68ac23d9f47e95a6d83d4c30b03db1535403f403
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-17 10:44:49 +0330

    FilterEverythingAlsoData is separated from it's component

--- src/lib/FilterEverythingAlsoData.js
+++ src/lib/FilterEverythingAlsoData.js
@@ -1,3 +1,10 @@
+import puzzleIcon from "../assets/images/FilterEverythingAlsoCards-icons/puzzle.svg";
+import ajaxIcon from "../assets/images/FilterEverythingAlsoCards-icons/ajax.svg";
+import developersFriendlyIcon from "../assets/images/FilterEverythingAlsoCards-icons/developersFriendly.svg";
+import wordpressIcon from "../assets/images/FilterEverythingAlsoCards-icons/wordpress.svg";
+import airplaneIcon from "../assets/images/FilterEverythingAlsoCards-icons/airplane.svg";
+import radioButtonIcon from "../assets/images/FilterEverythingAlsoCards-icons/radioButton.svg";
+
 const FilterEverythingAlsoData = [
   {
     id: 1,

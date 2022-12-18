2cf52360f62b45fa256a9ff67ff756b5ad9dbd79
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-18 09:35:22 +0330

    Changed the margin and padding

--- src/components/Body/ComparisonTable.jsx
+++ src/components/Body/ComparisonTable.jsx
@@ -5,8 +5,8 @@ import { ReactComponent as CrossIcon } from "../../assets/images/cross.svg";
 
 function ComparisonTable() {
   return (
-    <section className="flex flex-col gap-4 w-full bg-white md:pt-36 max-md:pt-8 max-md:pb-4 md: pb-8 border-t-2 border-[#e5e5e5]">
-      <div className="flex flex-col md:gap-2 items-center lg:px-[25%] max-lg:px-4 mb-6">
+    <section className="flex flex-col gap-4 w-full bg-white md:pt-36 max-md:pt-8 max-md:pb-4 md:pb-8 border-t-2 border-[#e5e5e5]">
+      <div className="flex flex-col md:gap-2 items-center lg:px-[25%] max-lg:px-4">
         <h2 className=" max-md:text-2xl md:text-lg lg:text-3xl font-bold mb-4">غول فیلتر</h2>
         <h2 className=" max-md:text-2xl md:text-lg lg:text-3xl font-bold max-lg:mb-4 lg:mb-8">تفاوت نسخه رایگان با نسخه حرفه ای</h2>
         <table className="max-md:mb-4 md:mb-8 lg:mb-40 ">

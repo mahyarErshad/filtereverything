48e0757e0c10909d2215282687206ce5a2e0531c
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-18 09:40:56 +0330

    py has been increased

--- src/components/Body/MakeVisitorsHappy.jsx
+++ src/components/Body/MakeVisitorsHappy.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function MakeVisitorsHappy() {
   return (
-    <section className='flex justify-center items-center py-12 bg-purple'>
+    <section className='flex justify-center items-center py-28 bg-purple'>
     <h3 className='text-white font-bold max-md:text-xl md:text-3xl lg:text-4xl'>بازدید کنندگان خود را راضی کنید با</h3>
     </section>
   )

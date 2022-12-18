eb6981fbea296680db1d777859602d948fdc3dc9
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-18 10:03:09 +0330

    Added 2 buttons

--- src/components/Body/MakeVisitorsHappy.jsx
+++ src/components/Body/MakeVisitorsHappy.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import Button from "../Utils/Button/Button"
+import React from "react";
+import Button from "../Utils/Button/Button";
 
 function MakeVisitorsHappy() {
   return (
-    <section className='flex justify-center items-center py-28 bg-purple'>
-    <h3 className='text-white font-bold max-md:text-xl md:text-3xl lg:text-4xl'>بازدید کنندگان خود را راضی کنید با</h3>
-    <div>
-        <Button />
-    </div>
+    <section className="flex justify-center items-center py-28 bg-purple">
+      <h3 className="text-white font-bold max-md:text-xl md:text-3xl lg:text-4xl">بازدید کنندگان خود را راضی کنید با</h3>
+      <div className="flex justify-between items-center flex-wrap gap-4">
+        <Button title="دانلود رایگان" className="download-for-free-button py-2" backgroundColor="bg-transparent" textColor="text-white" />
+        <Button title="خرید نسخه حرفه ای" className="hover:bg-[#18caca] font-bold py-[1.05rem] max-md:px-11" />
+      </div>
     </section>
-  )
+  );
 }
 
-export default MakeVisitorsHappy
+export default MakeVisitorsHappy;

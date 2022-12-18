0f3a406cf687c2365c226af0b2fdcf465d1ede0d
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-18 10:07:22 +0330

    Changed the button text

--- src/components/Body/MakeVisitorsHappy.jsx
+++ src/components/Body/MakeVisitorsHappy.jsx
@@ -6,7 +6,7 @@ function MakeVisitorsHappy() {
     <section className="flex justify-center items-center py-28 bg-purple">
       <h3 className="text-white font-bold max-md:text-xl md:text-3xl lg:text-4xl">بازدید کنندگان خود را راضی کنید با</h3>
       <div className="flex justify-between items-center flex-wrap gap-4">
-        <Button title="دانلود رایگان" className="py-2" backgroundColor="bg-transparent" textColor="text-white" />
+        <Button title="نسخه رایگان" className="py-2" backgroundColor="bg-transparent" textColor="text-white" />
         <Button title="نسخه حرفه ای" className="hover:bg-[#18caca] font-bold py-[1.05rem] max-md:px-11 md:px-14" />
       </div>
     </section>

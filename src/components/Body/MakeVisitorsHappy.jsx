af553d759d097dea6120e6a09f2b1609d5cbd853
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-18 09:40:04 +0330

    h3 is responsive

--- src/components/Body/MakeVisitorsHappy.jsx
+++ src/components/Body/MakeVisitorsHappy.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function MakeVisitorsHappy() {
   return (
     <section className='flex justify-center items-center py-12 bg-purple'>
-
+    <h3 className='text-white font-bold max-md:text-xl md:text-3xl lg:text-4xl'>بازدید کنندگان خود را راضی کنید با</h3>
     </section>
   )
 }

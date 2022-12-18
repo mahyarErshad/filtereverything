1dc83c4314df4589ac786926a60461d0005fb950
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-18 10:21:21 +0330

    section has 1 rem padding x

--- src/components/Body/MakeVisitorsHappy.jsx
+++ src/components/Body/MakeVisitorsHappy.jsx
@@ -3,7 +3,7 @@ import Button from "../Utils/Button/Button";
 
 function MakeVisitorsHappy() {
   return (
-    <section className="flex justify-center items-center py-28 bg-purple max-md:gap-4 md:gap-8 lg:gap-16 max-md:flex-col">
+    <section className="flex justify-center items-center py-28 bg-purple max-md:gap-4 md:gap-8 lg:gap-16 max-md:flex-col px-4">
       <h3 className="text-white font-bold max-md:text-xl md:text-3xl lg:text-4xl whitespace-nowrap">بازدید کنندگان خود را راضی کنید با</h3>
       <div className="flex justify-between items-center flex-wrap gap-4">
         <Button title="نسخه رایگان" className="make-visitors-happy__free-version max-md:px-11 md:px-12 py-[0.75rem]" backgroundColor="bg-transparent" textColor="text-white" />

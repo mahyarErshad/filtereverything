bcada8632d80daa2ad1a05b8d0022f1fbe2bd2de
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-18 09:36:56 +0330

    Background is purple

--- src/components/Body/MakeVisitorsHappy.jsx
+++ src/components/Body/MakeVisitorsHappy.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function MakeVisitorsHappy() {
   return (
-    <section className='flex justify-center items-center py-12'>
+    <section className='flex justify-center items-center py-12 bg-purple'>
 
     </section>
   )

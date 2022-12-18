7b3c6a7ebdf1d1c65d615ed229a99b40f0254f06
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-18 09:42:32 +0330

    Import the button

--- src/components/Body/MakeVisitorsHappy.jsx
+++ src/components/Body/MakeVisitorsHappy.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Button from "../Utils/Button/Button"
 
 function MakeVisitorsHappy() {
   return (
     <section className='flex justify-center items-center py-28 bg-purple'>
     <h3 className='text-white font-bold max-md:text-xl md:text-3xl lg:text-4xl'>بازدید کنندگان خود را راضی کنید با</h3>
+    <div>
+        <Button />
+    </div>
     </section>
   )
 }

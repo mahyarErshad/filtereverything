145fdc3b651cfb4af3f812eb648c1901ed470e3b
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-19 10:29:06 +0330

    FooterNavLink is fully responsive

--- src/components/Footer/FooterNavLink.jsx
+++ src/components/Footer/FooterNavLink.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function FooterNavLink({ data }) {
   return (
-    <div className="flex flex-col gap-2 max-lg:w-2/4 lg:w-1/4 max-md:mb-6 md:mb-8 lg:mb-0 max-sm:pr-3">
+    <div className="flex flex-col gap-2 max-lg:w-2/4 lg:w-1/4 max-md:mb-6 md:mb-8 lg:mb-0 max-sm:pr-3 md:pr-[12%] md:whitespace-nowrap">
       {data.map((item, index) => {
         return (
           <nav key={index} className="text-sm cursor-pointer hover:text-[#e6e6e6] transition-all">

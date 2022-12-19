9ba8a0ce62b9a368d9eb7dda2c08700630891222
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-19 09:56:25 +0330

    Copied codes to a new component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,9 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <footer className='py-14 bg-[#302f33] text-[#d4d4d4] flex flex-wrap justify-center'>
-    <div className='flex flex-col gap-4 max-md:w-full md:w-2/4 lg:w-1/4'>
-        <nav className='text-sm cursor-pointer hover:text-[#e6e6e6] transition-all'>نسخه حرفه ایی غول فیلتر</nav>
-    </div>
+
     </footer>
   )
 }
--- src/components/Footer/FooterNavLink.jsx
+++ src/components/Footer/FooterNavLink.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function FooterNavLink() {
   return (
-    <div>FooterNavLink</div>
-  )
+    <div className="flex flex-col gap-4 max-md:w-full md:w-2/4 lg:w-1/4">
+      <nav className="text-sm cursor-pointer hover:text-[#e6e6e6] transition-all">نسخه حرفه ایی غول فیلتر</nav>
+    </div>
+  );
 }
 
-export default FooterNavLink
+export default FooterNavLink;

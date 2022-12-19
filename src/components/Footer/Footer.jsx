ca71aed6bceba54813cbddf76d31d0fdae87d70d
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-19 09:59:50 +0330

    First data set has been added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import FooterNavLink from "./FooterNavLink";
 
 function Footer() {
   return (
-    <footer className='py-14 bg-[#302f33] text-[#d4d4d4] flex flex-wrap justify-center'>
-
+    <footer className="py-14 bg-[#302f33] text-[#d4d4d4] flex flex-wrap justify-center">
+      <FooterNavLink data={["نسخه حرفه ایی غول فیلتر", "درباره ما"]} />
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;

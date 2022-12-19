310686232610919ea8b0d87044cebb18bb29daee
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-19 12:04:05 +0330

    Worked on font weight

--- src/components/Header/HeaderTextSection.jsx
+++ src/components/Header/HeaderTextSection.jsx
@@ -13,10 +13,9 @@ function HeaderTextSection() {
         <h2 className="md:text-[4.5rem] sm:text-[3.5rem] max-sm:text-[3rem] text-white font-bold mr-3">فیلتر</h2>
       </div>
       <div className="flex flex-col md:gap-3 max-md:gap-2 md:mt-8 max-md:mt-6">
-        <p className="max-md:text-4xl md:text-5xl font-bold text-white">برای وردپرس</p>
-        <span className="md:-translate-y-3 max-md:-translate-y-2 text-white max-md:text-5xl md:text-6xl max-md:pr-20 md:pr-28">و</span>
+        <p className="max-md:text-4xl md:text-[4.5rem] font-bold text-white"><span className="font-light">برای</span> وردپرس <span className="font-light">و</span></p>
         <div className="max-md:text-3xl md:text-4xl font-bold text-white">
-          <img src={woocommerceLogoPng} className="max-md:w-[17rem] md:w-[21.5rem]" alt="woocommerce" />
+          <img src={woocommerceLogoPng} className="max-md:w-[17rem] md:w-[29rem]" alt="woocommerce" />
         </div>
       </div>
       <div className="max-md:w-[16.5rem] max-md:text-justify md:w-[22rem]">

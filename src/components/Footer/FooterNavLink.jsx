a945fa4433076d2ca4b9f87cfa9a69e67a53c907
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-19 10:03:53 +0330

    Gap has decreased

--- src/components/Footer/FooterNavLink.jsx
+++ src/components/Footer/FooterNavLink.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function FooterNavLink({ data }) {
   return (
-    <div className="flex flex-col gap-4 max-md:w-full md:w-2/4 lg:w-1/4">
+    <div className="flex flex-col gap-2 max-md:w-full md:w-2/4 lg:w-1/4">
       {data.map((item, index) => {
         return (
           <nav key={index} className="text-sm cursor-pointer hover:text-[#e6e6e6] transition-all">

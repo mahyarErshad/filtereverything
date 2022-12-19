8d34ea0c44673a511c2dd0c7e4471187c8dee015
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-19 10:00:52 +0330

    Second data set has been added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,6 +5,7 @@ function Footer() {
   return (
     <footer className="py-14 bg-[#302f33] text-[#d4d4d4] flex flex-wrap justify-center">
       <FooterNavLink data={["نسخه حرفه ایی غول فیلتر", "درباره ما"]} />
+      <FooterNavLink data={["مستندات", "پشتیبانی", "تماس با ما"]} />
     </footer>
   );
 }

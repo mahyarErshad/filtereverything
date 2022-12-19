7b4d2a783a09609cb00235c773e3bcf692ac3797
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-19 09:44:25 +0330

    Import Footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { memo } from "react";
 import MainBody from "./components/Body/MainBody";
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 
 function App() {
   return <>
   <Header />
   <MainBody />
+  <Footer />
   </>;
 }
 

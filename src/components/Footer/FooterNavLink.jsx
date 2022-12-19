a6f052184ffa2b5da6897f865bd45cc0e2808f17
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-19 09:58:28 +0330

    FooterNavLink creates footer links using data prop

--- src/components/Footer/FooterNavLink.jsx
+++ src/components/Footer/FooterNavLink.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 
-function FooterNavLink() {
+function FooterNavLink({ data }) {
   return (
     <div className="flex flex-col gap-4 max-md:w-full md:w-2/4 lg:w-1/4">
-      <nav className="text-sm cursor-pointer hover:text-[#e6e6e6] transition-all">نسخه حرفه ایی غول فیلتر</nav>
+      {data.map((item, index) => {
+        return (
+          <nav key={index} className="text-sm cursor-pointer hover:text-[#e6e6e6] transition-all">
+            {item}
+          </nav>
+        );
+      })}
     </div>
   );
 }

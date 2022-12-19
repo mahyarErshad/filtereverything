2f838264ae8cb6d116af0fca3572787c3712da92
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-19 10:23:58 +0330

    Fixed a bug

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import FooterNavLink from "./FooterNavLink";
 
 function Footer() {
   return (
-    <footer className="py-14 bg-[#302f33] text-[#d4d4d4] flex justify-center flex-wrap pr-[15%]">
+    <footer className="py-14 bg-[#302f33] text-[#d4d4d4] flex justify-center flex-wrap px-[15%]">
       <FooterNavLink data={["نسخه حرفه ایی غول فیلتر", "درباره ما"]} />
       <FooterNavLink data={["مستندات", "پشتیبانی", "تماس با ما"]} />
       <FooterNavLink data={["شرایط استفاده", "تغییرات"]} />

dc977c3ad48c90e41c63a53679ed1468f880f215
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-19 10:08:21 +0330

    Defined gaps

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import FooterNavLink from "./FooterNavLink";
 
 function Footer() {
   return (
-    <footer className="py-14 bg-[#302f33] text-[#d4d4d4] flex flex-wrap justify-center px-[15%]">
+    <footer className="py-14 bg-[#302f33] text-[#d4d4d4] flex flex-wrap justify-center px-[15%] max-md:gap-4 md:gap-6 lg:gap-0">
       <FooterNavLink data={["نسخه حرفه ایی غول فیلتر", "درباره ما"]} />
       <FooterNavLink data={["مستندات", "پشتیبانی", "تماس با ما"]} />
       <FooterNavLink data={["شرایط استفاده", "تغییرات"]} />

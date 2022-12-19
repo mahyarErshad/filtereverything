954560ac4b45e2bb4bd6e9431e8f815ffbb6fbfc
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-19 13:21:39 +0330

    p tag is responsive on small screens

--- src/components/Header/HeaderTextSection.jsx
+++ src/components/Header/HeaderTextSection.jsx
@@ -7,18 +7,18 @@ function HeaderTextSection() {
     <div className="md:pr-[5%] sm:pr-10 max-sm:pr-8 max-lg:bg-[#435ec3b9] pb-6 rounded-bl-[50%]">
       <div className="pt-16">
         <span className="text-center text-black max-lg:py-2 lg:py-0 px-4 rounded-[35px] bg-[#ffd000] sm:text-base md:text-xl lg:text-3xl font-bold">+سئو</span>
-        <h1 className="md:text-[6.7rem] max-md:text-[3.5rem] text-white font-bold max-md:mt-2">غول فیلتر</h1>
+        <h1 className="md:text-[6.7rem] max-md:text-[5rem] text-white font-bold max-md:mt-2">غول فیلتر</h1>
       </div>
       <div className="flex flex-col md:gap-3 max-md:gap-2">
-        <p className="max-md:text-[2.4rem] md:text-[4.5rem] font-bold text-white">
+        <p className="max-md:text-[3.4rem] md:text-[4.5rem] font-bold text-white">
           <span className="font-light">برای</span> وردپرس <span className="font-light">و</span>
         </p>
         <div className="max-md:text-3xl md:text-4xl font-bold text-white">
-          <img src={woocommerceLogoPng} className="max-md:w-[15.6rem] md:w-[29rem]" alt="woocommerce" />
+          <img src={woocommerceLogoPng} className="max-md:w-[22rem] md:w-[29rem]" alt="woocommerce" />
         </div>
       </div>
       <div className="max-md:w-[16.5rem] max-md:text-justify md:w-[22rem]">
-        <p className="text-white mt-4 max-md:text-justify max-md:w-[15rem] md:w-[29rem]">غول فیلتر - افزونه فیلتر محصولات وردپرس و ووکامرس که به شما اجازه پیاده سازی هر نوع فیلتری را می دهد.</p>
+        <p className="text-white mt-4 max-md:text-justify max-md:w-[21rem] md:w-[29rem]">غول فیلتر - افزونه فیلتر محصولات وردپرس و ووکامرس که به شما اجازه پیاده سازی هر نوع فیلتری را می دهد.</p>
       </div>
       <div className="flex justify-between items-center flex-wrap mt-6 max-md:w-[16.5rem] md:w-[26rem] gap-4">
         <Button title="دانلود رایگان" className="download-for-free-button py-2" backgroundColor="bg-transparent" textColor="text-white" />

1dbe1b854c50b025b2cbcb7d5fa57f009a9c5694
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-20 09:08:50 +0330

    On hover styles have been added

--- src/components/Utils/Button/GoToTopButton.jsx
+++ src/components/Utils/Button/GoToTopButton.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function GoToTopButton() {
   return (
-    <button className="cursor-pointer rounded-[50%] p-2 flex justify-center items-center border border-white fixed bottom-[5%] left-[5%] bg-purple">
-      <span class="material-symbols-outlined text-white">arrow_upward</span>
+    <button className="cursor-pointer rounded-[50%] p-2 flex justify-center items-center border border-white fixed bottom-[5%] left-[5%] bg-purple transition-all hover:bg-[#1f3fca] text-white">
+      <span class="material-symbols-outlined text-inherit">arrow_upward</span>
     </button>
   );
 }

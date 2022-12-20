3ab84de375fe6f037821a67b1e0faf37c9bd0089
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-20 09:20:06 +0330

    Defined a new state

--- src/components/Utils/Button/GoToTopButton.jsx
+++ src/components/Utils/Button/GoToTopButton.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
 function GoToTopButton() {
+    const [scrollPosition, setScrollPosition] = useState(0)
   return (
     <button className="cursor-pointer rounded-[50%] p-2 flex justify-center items-center border border-white fixed bottom-[5%] left-[5%] bg-purple transition-all hover:bg-[#1f3fca] text-white">
       <span class="material-symbols-outlined text-inherit">arrow_upward</span>

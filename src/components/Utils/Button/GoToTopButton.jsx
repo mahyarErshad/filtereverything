02a3e675eccaf125910a34731b6411b374c258d7
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-20 09:22:20 +0330

    Changed class to className

--- src/components/Utils/Button/GoToTopButton.jsx
+++ src/components/Utils/Button/GoToTopButton.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 
 function GoToTopButton() {
-    const [scrollPosition, setScrollPosition] = useState(0)
+  const position = window.pageYOffset;
+  console.log(position);
   return (
     <button className="cursor-pointer rounded-[50%] p-2 flex justify-center items-center border border-white fixed bottom-[5%] left-[5%] bg-purple transition-all hover:bg-[#1f3fca] text-white">
-      <span class="material-symbols-outlined text-inherit">arrow_upward</span>
+      <span className="material-symbols-outlined text-inherit">arrow_upward</span>
     </button>
   );
 }

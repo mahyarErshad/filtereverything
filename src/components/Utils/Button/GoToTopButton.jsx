70c8d887272d2bbadef823c5f0b778de5ed9ce18
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-20 08:52:45 +0330

    Added the styles

--- src/components/Utils/Button/GoToTopButton.jsx
+++ src/components/Utils/Button/GoToTopButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function GoToTopButton() {
   return (
-    <button>
+    <button className="cursor-pointer border-r-[50%] text-center fixed bottom-[5%] left-[90%]">
       <span class="material-symbols-outlined">arrow_upward</span>
     </button>
   );

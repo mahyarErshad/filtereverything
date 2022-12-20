c353e1d08027be2025cb35a40a43d2c33fc57869
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-20 10:20:37 +0330

    Scroll to top function is working

--- src/components/Utils/Button/GoToTopButton.jsx
+++ src/components/Utils/Button/GoToTopButton.jsx
@@ -7,7 +7,6 @@ function GoToTopButton() {
       behavior: "smooth",
     });
   }
-
   return (
     <button onClick={handleScroll} className="cursor-pointer rounded-[50%] p-2 flex justify-center items-center border border-white fixed bottom-[5%] left-[5%] bg-purple transition-all hover:bg-[#1f3fca] text-white">
       <span className="material-symbols-outlined text-inherit">arrow_upward</span>

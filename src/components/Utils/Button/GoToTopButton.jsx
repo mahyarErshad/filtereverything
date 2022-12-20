925312cfcaea04d6c2262cfbe5b20dc08d2d179d
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-20 13:58:28 +0330

    Getting height of the scroll is successful

--- src/components/Utils/Button/GoToTopButton.jsx
+++ src/components/Utils/Button/GoToTopButton.jsx
@@ -1,12 +1,26 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function GoToTopButton() {
+  const [scrollHeight, setScrollHeight] = useState(false);
+  const root = document.getElementById("root");
   function handleScroll() {
-    document.getElementById("root").scroll({
+    root.scroll({
       top: 0,
       behavior: "smooth",
     });
   }
+  useEffect(() => {
+    root.addEventListener("scroll", () => {
+      if (root.scrollTop > 1000) {
+        setScrollHeight(true);
+      } else {
+        setScrollHeight(false);
+      }
+    }); // eslint-disable-next-line
+  }, [scrollHeight]);
+
   return (
     <>
       <button onClick={handleScroll} className="cursor-pointer rounded-[50%] p-2 flex justify-center items-center border border-white fixed bottom-[5%] left-[5%] bg-purple transition-all hover:bg-[#1f3fca] text-white">

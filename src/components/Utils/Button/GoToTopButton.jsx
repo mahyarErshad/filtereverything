66119a75a22aa11c0b41cc3918551e697187a8da
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-21 09:07:08 +0330

    Removed unneeded styles

--- src/components/Utils/Button/GoToTopButton.jsx
+++ src/components/Utils/Button/GoToTopButton.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect, useRef } from "react";
-import { CSSTransition } from "react-transition-group";
+import { Transition } from "react-transition-group";
 
 function GoToTopButton() {
   const [scrollHeight, setScrollHeight] = useState(false);
-  const nodeRef = useRef(null)
+  const nodeRef = useRef(null);
   const root = document.getElementById("root");
   function handleScroll() {
     root.scroll({
@@ -23,11 +23,11 @@ function GoToTopButton() {
 
   return (
     <>
-      <CSSTransition nodeRef={nodeRef} timeout={500} in={scrollHeight} classNames={"GoToTopButton"} unmountOnExit>
+      <Transition nodeRef={nodeRef} timeout={2000} in={scrollHeight} classNames="GoToTopButton" unmountOnExit>
         <button onClick={handleScroll} className="cursor-pointer rounded-[50%] p-2 flex justify-center items-center border border-white fixed bottom-[5%] left-[5%] bg-purple transition-all hover:bg-[#1f3fca] text-white">
           <span className="material-symbols-outlined text-inherit">arrow_upward</span>
         </button>
-      </CSSTransition>
+      </Transition>
     </>
   );
 }

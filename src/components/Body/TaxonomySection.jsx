03d0b2200e76e72c1d002ac715b0efe843751771
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-24 09:54:00 +0330

    Using purple text instead of bold

--- src/components/Body/TaxonomySection.jsx
+++ src/components/Body/TaxonomySection.jsx
@@ -9,7 +9,7 @@ function TaxonomySection() {
         <p className="font-bold max-lg:text-xl lg:text-3xl whitespace-nowrap">
           فیلترهای بر اساس <span className="text-purple">طبقه بندی</span>
         </p>
-        <p className="font-bold max-lg:text-xl lg:text-3xl">و فیلدهای سفارشی</p>
+        <p className="font-bold max-lg:text-xl lg:text-3xl">و <span className="text-purple">فیلدهای سفارشی</span></p>
         <p className="text-base text-[#676767] font-medium mt-8">این ویژگی 99 درصد نیازهای فیلترینگ احتمالی را پوشش می دهد.</p>
       </VerticalContentLayout>
     </>

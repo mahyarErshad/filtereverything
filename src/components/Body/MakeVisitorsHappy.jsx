8f8e44180ad05d8c06b12704863c3ffd7e3d523d
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-24 10:56:20 +0330

    MakeVisitorsHappy is responsive again

--- src/components/Body/MakeVisitorsHappy.jsx
+++ src/components/Body/MakeVisitorsHappy.jsx
@@ -3,7 +3,7 @@ import Button from "../Utils/Button/Button";
 
 function MakeVisitorsHappy() {
   return (
-    <section className="flex justify-center items-center py-28 bg-purple max-md:gap-6 md:gap-8 lg:gap-16 max-md:flex-col px-4">
+    <section className="flex justify-center flex-wrap items-center py-28 bg-purple max-md:gap-6 md:gap-8 lg:gap-16 max-md:flex-col px-4">
       <h3 className="text-white font-bold max-md:text-xl md:text-3xl lg:text-4xl whitespace-nowrap">بازدید کنندگان خود را راضی کنید با</h3>
       <div className="flex justify-center items-center flex-wrap gap-4">
         <Button href="https://www.zhaket.com/web/filter-everything-filter-plugin" title="مشاهده پیش نمایش" className="download-for-free-button py-[0.85rem] max-sm:w-[21rem] max-md:px-[1.8rem] md:px-[1rem]" backgroundColor="bg-transparent" textColor="text-white" />

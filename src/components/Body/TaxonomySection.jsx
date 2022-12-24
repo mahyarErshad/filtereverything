8d092300706e0ddac8f8cc640fb0ead9680bce19
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-24 09:07:23 +0330

    Removed self center for first child

--- src/components/Body/TaxonomySection.jsx
+++ src/components/Body/TaxonomySection.jsx
@@ -6,7 +6,7 @@ function TaxonomySection() {
   return (
     <>
       <VerticalContentLayout image={taxonomyImage} alt="taxonomy" >
-        <p className="font-bold max-lg:text-xl lg:text-3xl break-keep self-center">فیلترهای بر اساس طبقه بندی</p>
+        <p className="font-bold max-lg:text-xl lg:text-3xl break-keep">فیلترهای بر اساس طبقه بندی</p>
         <p className="font-bold max-lg:text-xl lg:text-3xl">و فیلدهای سفارشی</p>
         <p className="text-base text-[#676767] font-medium mt-8">این ویژگی 99 درصد نیازهای فیلترینگ احتمالی را پوشش می دهد.</p>
       </VerticalContentLayout>

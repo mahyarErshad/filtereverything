2e93a762e523c712074ec90d53b3bc8a70aad467
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-24 10:54:17 +0330

    Changed classname of buttons

--- src/components/Body/MakeVisitorsHappy.jsx
+++ src/components/Body/MakeVisitorsHappy.jsx
@@ -6,8 +6,8 @@ function MakeVisitorsHappy() {
     <section className="flex justify-center items-center py-28 bg-purple max-md:gap-6 md:gap-8 lg:gap-16 max-md:flex-col px-4">
       <h3 className="text-white font-bold max-md:text-xl md:text-3xl lg:text-4xl whitespace-nowrap">بازدید کنندگان خود را راضی کنید با</h3>
       <div className="flex justify-center items-center flex-wrap gap-4">
-        <Button href="https://www.zhaket.com/web/filter-everything-filter-plugin" title="مشاهده پیش نمایش" className="make-visitors-happy__free-version max-md:px-11 md:px-12 py-[0.75rem]" backgroundColor="bg-transparent" textColor="text-white" />
-        <Button href="https://www.zhaket.com/web/filter-everything-filter-plugin" title="نسخه حرفه ای" className="hover:bg-[#18caca] font-bold py-[1.05rem] max-md:px-11 md:px-14" />
+        <Button href="https://www.zhaket.com/web/filter-everything-filter-plugin" title="مشاهده پیش نمایش" className="download-for-free-button py-[0.85rem] max-sm:w-[21rem] max-md:px-[1.8rem] md:px-[1rem]" backgroundColor="bg-transparent" textColor="text-white" />
+        <Button href="https://www.zhaket.com/web/filter-everything-filter-plugin" title="نسخه حرفه ای" className="hover:bg-[#18caca] font-bold py-[1.05rem] max-md:px-[3.2rem] md:px-[2rem] custom-border max-sm:w-[21rem]" />
       </div>
     </section>
   );

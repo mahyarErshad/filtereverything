d92ab85e5268da0616a3a3b18b711d9bfa254a8e
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-24 09:19:11 +0330

    Justify content center

--- src/components/Layout/VerticalContentLayout/VerticalContentLayout.jsx
+++ src/components/Layout/VerticalContentLayout/VerticalContentLayout.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function VerticalContentLayout(props) {
   return (
     <section className="flex flex-nowrap w-full px-4 lg:pr-[10%] max-md:flex-col max-sm:mt-4">
-      <div className="md:w-2/4 max-md:w-full flex flex-col max-lg:px-4 lg:pr-[8%] md:items-end justify-between max-md:py-4 md:py-8">
+      <div className="md:w-2/4 max-md:w-full flex flex-col max-lg:px-4 lg:pr-[8%] md:items-end justify-center max-md:py-4 md:pb-8">
         <div className="flex flex-col w-full gap-4 max-md:justify-center max-md:items-center md:items-start">
         {props.children}
         </div>

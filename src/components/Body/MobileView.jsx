347f6b649100fd765146b2f232ef474f77fc10c2
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-24 09:58:43 +0330

    Using VerticalContentLayout

--- src/components/Body/MobileView.jsx
+++ src/components/Body/MobileView.jsx
@@ -1,20 +1,16 @@
 import React, { memo } from "react";
 import mobileView from "../../assets/images/mobileView.jpg";
+import VerticalContentLayout from "../Layout/VerticalContentLayout/VerticalContentLayout";
 
 function MobileView() {
   return (
-    <section className="flex flex-nowrap w-full px-4 lg:px-[10%] max-md:flex-col max-sm:mt-4">
-      <div className="md:w-2/4 max-md:w-full flex flex-col max-lg:px-4 lg:pr-[8%] md:items-end justify-between max-md:py-4 md:pb-[7%] md:pt-[8%]">
-        <div className="flex flex-col w-full gap-4 max-md:justify-center max-md:items-center md:items-start">
-          <p className="font-bold max-lg:text-xl lg:text-3xl whitespace-nowrap">آماده استفاده بر روی</p>
-          <p className="font-bold max-lg:text-xl lg:text-3xl self-center">دستگاه های موبایل</p>
-          <p className="text-base text-[#676767] font-medium mt-8">ویجت مخصوص زیرین به کاربران اجازه می دهد محصولات مورد نیازشان را تنها در چند کلیک پیدا کنند.</p>
-        </div>
-      </div>
-      <div className="md:w-2/4 max-md:w-full flex max-md:justify-center p-[5%] pr-0">
-        <img className="h-auto max-w-full" src={mobileView} alt="taxonomy" />
-      </div>
-    </section>
+    <>
+      <VerticalContentLayout>
+        <p className="font-bold max-lg:text-xl lg:text-3xl whitespace-nowrap">آماده استفاده بر روی</p>
+        <p className="font-bold max-lg:text-xl lg:text-3xl self-center">دستگاه های موبایل</p>
+        <p className="text-base text-[#676767] font-medium mt-8">ویجت مخصوص زیرین به کاربران اجازه می دهد محصولات مورد نیازشان را تنها در چند کلیک پیدا کنند.</p>
+      </VerticalContentLayout>
+    </>
   );
 }
 

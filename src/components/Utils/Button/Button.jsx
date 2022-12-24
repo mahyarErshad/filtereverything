4e33f2739b7c3ce7b049c64f5e8ceb42be348d58
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-24 10:28:33 +0330

    Button takes href as prop

--- src/components/Utils/Button/Button.jsx
+++ src/components/Utils/Button/Button.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 
 function Button(props) {
-  const { title } = props;
-  const backgroundColor = props.backgroundColor || "bg-primary"
-  const textColor = props.textColor || "text-[#323232]"
+  const { title, href } = props;
+  const backgroundColor = props.backgroundColor || "bg-primary";
+  const textColor = props.textColor || "text-[#323232]";
   return (
-    <button className={`${backgroundColor} ${textColor} px-4 rounded-md text-ellipsis whitespace-nowrap transition-all ${props.className}`} onClick={props.onClick}>
-      {title}
-    </button>
+    <a href={href} target="_blank" without rel="noreferrer" >
+      <button className={`${backgroundColor} ${textColor} px-4 rounded-md text-ellipsis whitespace-nowrap transition-all ${props.className}`} onClick={props.onClick}>
+        {title}
+      </button>
+    </a>
   );
 }
 

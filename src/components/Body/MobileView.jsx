394f94f71ff8445ecc0d038ed025261c8329cbd0
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-24 10:00:47 +0330

    Refactored using VerticalContentLayout

--- src/components/Body/MobileView.jsx
+++ src/components/Body/MobileView.jsx
@@ -5,9 +5,8 @@ import VerticalContentLayout from "../Layout/VerticalContentLayout/VerticalConte
 function MobileView() {
   return (
     <>
-      <VerticalContentLayout>
-        <p className="font-bold max-lg:text-xl lg:text-3xl whitespace-nowrap">آماده استفاده بر روی</p>
-        <p className="font-bold max-lg:text-xl lg:text-3xl self-center">دستگاه های موبایل</p>
+      <VerticalContentLayout image={mobileView} alt="mobile phone">
+        <p className="font-bold max-lg:text-xl lg:text-3xl">آماده استفاده بر روی دستگاه های موبایل</p>
         <p className="text-base text-[#676767] font-medium mt-8">ویجت مخصوص زیرین به کاربران اجازه می دهد محصولات مورد نیازشان را تنها در چند کلیک پیدا کنند.</p>
       </VerticalContentLayout>
     </>

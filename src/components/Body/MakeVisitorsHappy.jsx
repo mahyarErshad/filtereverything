52ccc24faa611c9e64e001ebf3ee2a1ddbdc2a16
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-24 10:55:24 +0330

    Changed the title of the button

--- src/components/Body/MakeVisitorsHappy.jsx
+++ src/components/Body/MakeVisitorsHappy.jsx
@@ -7,7 +7,7 @@ function MakeVisitorsHappy() {
       <h3 className="text-white font-bold max-md:text-xl md:text-3xl lg:text-4xl whitespace-nowrap">بازدید کنندگان خود را راضی کنید با</h3>
       <div className="flex justify-center items-center flex-wrap gap-4">
         <Button href="https://www.zhaket.com/web/filter-everything-filter-plugin" title="مشاهده پیش نمایش" className="download-for-free-button py-[0.85rem] max-sm:w-[21rem] max-md:px-[1.8rem] md:px-[1rem]" backgroundColor="bg-transparent" textColor="text-white" />
-        <Button href="https://www.zhaket.com/web/filter-everything-filter-plugin" title="نسخه حرفه ای" className="hover:bg-[#18caca] font-bold py-[1.05rem] max-md:px-[3.2rem] md:px-[2rem] custom-border max-sm:w-[21rem]" />
+        <Button href="https://www.zhaket.com/web/filter-everything-filter-plugin" title="خرید نسخه حرفه ای" className="hover:bg-[#18caca] font-bold py-[1.05rem] max-md:px-[3.2rem] md:px-[2rem] custom-border max-sm:w-[21rem]" />
       </div>
     </section>
   );

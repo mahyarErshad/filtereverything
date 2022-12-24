bf60634974425b4f73903bdec3390db439ca45f2
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-24 08:54:58 +0330

    Copied the layout

--- src/components/Layout/VerticalContentLayout/VerticalContentLayout.jsx
+++ src/components/Layout/VerticalContentLayout/VerticalContentLayout.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function VerticalContentLayout() {
   return (
-    <div>VerticalContentLayout</div>
-  )
+    <section className="flex flex-nowrap w-full px-4 lg:px-[10%] max-md:flex-col max-sm:mt-4">
+      <div className="md:w-2/4 max-md:w-full flex flex-col max-lg:px-4 lg:pr-[8%] md:items-end justify-between max-md:py-4 md:pb-[7%] md:pt-[8%]">
+        <div className="flex flex-col w-full gap-4 max-md:justify-center max-md:items-center md:items-end"></div>
+      </div>
+    </section>
+  );
 }
 
-export default VerticalContentLayout
+export default VerticalContentLayout;

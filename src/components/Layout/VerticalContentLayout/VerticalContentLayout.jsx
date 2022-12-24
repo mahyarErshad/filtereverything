81a6afa4583249d9d78a0921fc1b1af8e208b80e
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-24 08:55:51 +0330

    Getting children from props

--- src/components/Layout/VerticalContentLayout/VerticalContentLayout.jsx
+++ src/components/Layout/VerticalContentLayout/VerticalContentLayout.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
-function VerticalContentLayout() {
+function VerticalContentLayout(props) {
   return (
     <section className="flex flex-nowrap w-full px-4 lg:px-[10%] max-md:flex-col max-sm:mt-4">
       <div className="md:w-2/4 max-md:w-full flex flex-col max-lg:px-4 lg:pr-[8%] md:items-end justify-between max-md:py-4 md:pb-[7%] md:pt-[8%]">
-        <div className="flex flex-col w-full gap-4 max-md:justify-center max-md:items-center md:items-end"></div>
+        <div className="flex flex-col w-full gap-4 max-md:justify-center max-md:items-center md:items-end">
+        {props.children}
+        </div>
       </div>
     </section>
   );

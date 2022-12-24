b532fe8256d923d558462494e6ec5fc8d99b39f9
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-24 10:29:35 +0330

    Passed href to the button

--- src/components/Body/MakeVisitorsHappy.jsx
+++ src/components/Body/MakeVisitorsHappy.jsx
@@ -7,7 +7,7 @@ function MakeVisitorsHappy() {
       <h3 className="text-white font-bold max-md:text-xl md:text-3xl lg:text-4xl whitespace-nowrap">بازدید کنندگان خود را راضی کنید با</h3>
       <div className="flex justify-center items-center flex-wrap gap-4">
         <Button title="نسخه رایگان" className="make-visitors-happy__free-version max-md:px-11 md:px-12 py-[0.75rem]" backgroundColor="bg-transparent" textColor="text-white" />
-        <Button title="نسخه حرفه ای" className="hover:bg-[#18caca] font-bold py-[1.05rem] max-md:px-11 md:px-14" />
+        <Button href="https://www.zhaket.com/web/filter-everything-filter-plugin" title="نسخه حرفه ای" className="hover:bg-[#18caca] font-bold py-[1.05rem] max-md:px-11 md:px-14" />
       </div>
     </section>
   );

9cac5007630ceaf935f60719556eef5c1f775624
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-26 09:12:33 +0330

    Fixed burger menu's bug

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,6 @@ import HeaderTextSection from "./HeaderTextSection";
 
 function Header() {
   const [openMenu, setOpenMenu] = useState(false);
-  const openMenuClass = openMenu ? "visible" : "hidden";
   return (
     <header className="header-container">
       <div className="header-menu flex justify-between max-sm:px-4 sm:px-[10%] md:px-[20%] text-prima relative h-16 ">
@@ -15,7 +14,7 @@ function Header() {
               {openMenu ? "close" : "menu"}
             </button>
           </div>
-          <div className={`flex justify-center z-10 max-sm:items-start max-md:pb-2 max-md:w-full max-md:bg-[#333333] max-md:opacity-[0.8] sm:pr-[6.5rem] max-sm:pr-12 sm:items-start gap-6 md:h-full sm:h-auto max-sm:h-auto items-center max-md:${openMenuClass} md:flex max-sm:flex-col sm:flex-col md:flex-row sm:absolute max-sm:absolute max-sm:top-[100%] max-sm:right-[0%] sm:top-[100%] sm:right-[0] md:top-[0%] `}>
+          <div className={`flex justify-center z-10 max-sm:items-start max-md:pb-2 max-md:w-full max-md:bg-[#333333] max-md:opacity-[0.8] sm:pr-[6.5rem] max-sm:pr-12 sm:items-start gap-6 md:h-full sm:h-auto max-sm:h-auto items-center ${openMenu ? "" : "max-md:hidden"} md:flex max-sm:flex-col sm:flex-col md:flex-row sm:absolute max-sm:absolute max-sm:top-[100%] max-sm:right-[0%] sm:top-[100%] sm:right-[0] md:top-[0%] `}>
             <li className="h-full text-[1.125rem] animation-fade font-bold flex items-center">پشتیبانی</li>
             <li className="h-full text-[1.125rem] animation-fade font-bold flex items-center">مستندات</li>
             <HeaderDropDownMenu />
